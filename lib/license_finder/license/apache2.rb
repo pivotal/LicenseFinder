8c1ff808d5e39b475087d6117c8fada884adf11d
Matthew Kane Parker <pair+mparker@pivotallabs.com>
2012-09-13 15:43:38 -0400

    rename Apache to Apache2

--- lib/license_finder/license/apache2.rb
+++ lib/license_finder/license/apache2.rb
@@ -1,4 +1,4 @@
-class LicenseFinder::License::Apache < LicenseFinder::License::Base
+class LicenseFinder::License::Apache2 < LicenseFinder::License::Base
   self.alternative_names = ["Apache 2.0", "Apache2"]
   self.license_url       = "http://www.apache.org/licenses/LICENSE-2.0.txt"
 
--- spec/lib/license_finder/license/apache_spec.rb
+++ spec/lib/license_finder/license/apache_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
-describe LicenseFinder::License::Apache do
-  subject { LicenseFinder::License::Apache.new("") }
+describe LicenseFinder::License::Apache2 do
+  subject { LicenseFinder::License::Apache2.new("") }
 
   it_behaves_like "a license matcher"
 end
