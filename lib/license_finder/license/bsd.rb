4272ed5ca02484f8199d0175db6dd5c969348a76
Benjamin Quorning <bquorning@zendesk.com>
2013-02-18 13:35:37 +0100

    Recognize identifiers from http://spdx.org/licenses/

--- lib/license_finder/license/bsd.rb
+++ lib/license_finder/license/bsd.rb
@@ -1,4 +1,4 @@
 class LicenseFinder::License::BSD < LicenseFinder::License::Base
-  self.alternative_names = ["BSD4", "bsd-old", "4-clause BSD"]
+  self.alternative_names = ["BSD4", "bsd-old", "4-clause BSD", "BSD-4-Clause"]
   self.license_url = "http://en.wikipedia.org/wiki/BSD_licenses#4-clause_license_.28original_.22BSD_License.22.29"
 end
--- lib/license_finder/license/new_bsd.rb
+++ lib/license_finder/license/new_bsd.rb
@@ -1,6 +1,6 @@
 class LicenseFinder::License::NewBSD < LicenseFinder::License::Base
   self.license_url = "http://opensource.org/licenses/BSD-3-Clause"
-  self.alternative_names = ["Modified BSD", "BSD3", "BSD-3", "3-clause BSD"]
+  self.alternative_names = ["Modified BSD", "BSD3", "BSD-3", "3-clause BSD", "BSD-3-Clause"]
 
   def self.pretty_name
     'New BSD'
