50e68b146e20fd5a1b910b2bbb5a60580d065626
David Tengdin & Jacob Maine <pair+dtengdin+jacob@pivotallabs.com>
2013-03-27 17:52:08 -0400

    refactor html tests to use Capybara

--- spec/lib/license_finder/html_report_spec.rb
+++ spec/lib/license_finder/html_report_spec.rb
@@ -5,15 +5,15 @@ module LicenseFinder
   describe HtmlReport do
     describe "#to_s" do
       let(:dependency) { Dependency.new 'approved' => true, 'name' => "the-name", 'license' => 'MIT' }
-      subject { HtmlReport.new([dependency]).to_s }
+      subject { Capybara.string(HtmlReport.new([dependency]).to_s) }
 
       context "when the dependency is approved" do
         it "should add an approved class to dependency's container" do
-          should include %{class="approved"}
+          should have_selector ".approved"
         end
 
         it "does not list the dependency in the action items" do
-          Capybara.string(subject).should_not have_selector ".action-items"
+          should_not have_selector ".action-items"
         end
       end
 
@@ -21,18 +21,18 @@ module LicenseFinder
         before { dependency.approved = false }
 
         it "should not add an approved class to he dependency's container" do
-          should include %{class="unapproved"}
+          should have_selector ".unapproved"
         end
 
         it "lists the dependency in the action items" do
-          Capybara.string(subject).should have_selector ".action-items li"
+          should have_selector ".action-items li"
         end
       end
 
       context "when the gem has at least one bundler group" do
         before { dependency.bundler_groups = ["group"] }
         it "should show the bundler group(s) in parens" do
-          should include "(group)"
+          should have_text "(group)"
         end
       end
 
@@ -40,7 +40,7 @@ module LicenseFinder
         before { dependency.bundler_groups = [] }
 
         it "should not show any parens or bundler group info" do
-          should_not include "()"
+          should_not have_text "()"
         end
 
       end
@@ -48,13 +48,13 @@ module LicenseFinder
       context "when the gem has at least one parent" do
         before { dependency.parents = [OpenStruct.new(:name => "foo parent")] }
         it "should include a parents section" do
-          should include "Parents"
+          should have_text "Parents"
         end
       end
 
       context "when the gem has no parents" do
         it "should not include any parents section in the output" do
-          should_not include "Parents"
+          should_not have_text "Parents"
         end
       end
 
@@ -62,13 +62,13 @@ module LicenseFinder
         before { dependency.children = [OpenStruct.new(:name => "foo child")] }
 
         it "should include a Children section" do
-          should include "Children"
+          should have_text "Children"
         end
       end
 
       context "when the gem has no children" do
         it "should not include any Children section in the output" do
-          should_not include "Children"
+          should_not have_text "Children"
         end
       end
     end
