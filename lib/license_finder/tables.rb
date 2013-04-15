eef809e1259f3956e8d6686102991e2170f6738f
Jeremy Wadsack <jeremy.wadsack@gmail.com>
2013-04-15 09:15:36 -0700

    Fix Sequel loader to escape paths with spaces in them

--- lib/license_finder/tables.rb
+++ lib/license_finder/tables.rb
@@ -2,6 +2,7 @@ require 'rubygems'
 require 'sequel'
 require LicenseFinder::Platform.sqlite_load_path
 
+#DB = Sequel.connect(URI.escape("#{LicenseFinder::Platform.sqlite_adapter}://#{LicenseFinder.config.database_path}"))
 DB = Sequel.connect("#{LicenseFinder::Platform.sqlite_adapter}://#{LicenseFinder.config.database_path}")
 Sequel.extension :migration, :core_extensions
 Sequel::Migrator.run(DB, LicenseFinder::ROOT_PATH.join('../db/migrate'))
