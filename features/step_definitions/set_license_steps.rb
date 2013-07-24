ad6dfd4e46d5e0a54b65c60b9d69a05b6426f9f8
Paul Meskers and William Ramsey <pair+pmeskers+wramsey@pivotallabs.com>
2013-07-24 17:46:45 -0400

    Improve set_license_steps to check for license

--- features/step_definitions/set_license_steps.rb
+++ features/step_definitions/set_license_steps.rb
@@ -2,15 +2,15 @@ Given(/^I have an app with license finder that depends on an other licensed gem$
   @user = ::DSL::User.new
   @user.create_nonrails_app
   @user.add_license_finder_to_rakefile
-  @user.add_dependency_to_app 'other_gem', :license => 'other'
+  @user.add_dependency_to_app 'other_gem', version: '1.0', license: 'other'
 end
 
 When(/^I set that gems license to MIT from the command line$/) do
-  @output = @user.execute_command 'license_finder --quiet'
-  @output = @user.execute_command 'license_finder license MIT other_gem'
+  @user.execute_command 'license_finder --quiet'
+  @user.execute_command 'license_finder license MIT other_gem'
   @output = @user.execute_command 'license_finder --quiet'
 end
 
 Then(/^I should see that other gems license set to MIT$/) do
-  @output.should include 'other_gem'
+  @output.should include 'other_gem, 1.0, MIT'
 end
