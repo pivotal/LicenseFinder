15ebfd8bb118fa3f18dc916427e68975e03c3558
grosser <michael@grosser.it>
2013-12-19 17:08:29 -0800

    another alias for apache v2

--- lib/license_finder/license/apache2.rb
+++ lib/license_finder/license/apache2.rb
@@ -1,5 +1,5 @@
 class LicenseFinder::License::Apache2 < LicenseFinder::License::Base
-  self.alternative_names = ["Apache 2.0", "Apache2", "Apache-2.0", "Apache Software License", "Apache License 2.0"]
+  self.alternative_names = ["Apache 2.0", "Apache2", "Apache-2.0", "Apache Software License", "Apache License 2.0", "Apache License Version 2.0"]
   self.license_url       = "http://www.apache.org/licenses/LICENSE-2.0.txt"
 
   def self.pretty_name
