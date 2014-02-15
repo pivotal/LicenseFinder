26c051375ad14874e7e2349be29bc149153c2bb0
Jacob Maine <jacob.maine@gmail.com>
2014-02-15 18:15:18 +0200

    Remove more stubbing

--- spec/lib/license_finder_spec.rb
+++ spec/lib/license_finder_spec.rb
@@ -2,28 +2,15 @@ require 'spec_helper'
 
 describe LicenseFinder do
   describe ".config" do
-    let(:config) do
-      {
-        'whitelist' => %w(MIT Apache),
-        'ignore_groups' => %w(test development)
-      }
-    end
-
-    before do
-      LicenseFinder.instance_variable_set(:@config, nil)
-      LicenseFinder::Configuration.stub(:config_file_exists?).and_return(true)
-      LicenseFinder::Configuration.stub(:persisted_config_hash).and_return(config)
-    end
-
-    after do
-      LicenseFinder.instance_variable_set(:@config, nil)
-    end
-
     it "should load the configuration exactly once" do
-      LicenseFinder::Configuration.should_receive(:persisted_config_hash).once.and_return(config)
+      LicenseFinder.instance_variable_set(:@config, nil)
 
-      LicenseFinder.config.whitelist
-      LicenseFinder.config.whitelist
+      LicenseFinder::Configuration.should_receive(:ensure_default).once.and_return(double(:config))
+
+      LicenseFinder.config
+      LicenseFinder.config
+
+      LicenseFinder.instance_variable_set(:@config, nil)
     end
   end
 end
