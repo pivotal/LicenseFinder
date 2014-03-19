7608a6d9a5c10ad7956fed9b627517d7e0ceba29
Jacob Maine <jacob.maine@gmail.com>
2014-03-19 16:31:21 +0300

    Fix indentation

--- spec/lib/license_finder/possible_license_file_spec.rb
+++ spec/lib/license_finder/possible_license_file_spec.rb
@@ -1,38 +1,38 @@
 require 'spec_helper'
 
 module LicenseFinder
-describe PossibleLicenseFile do
-  context "file parsing" do
-    subject { described_class.new('root', 'root/nested/path') }
+  describe PossibleLicenseFile do
+    context "file parsing" do
+      subject { described_class.new('root', 'root/nested/path') }
 
-    context "ignoring text" do
+      context "ignoring text" do
+        before do
+          subject.stub(:text).and_return('file text')
+        end
+
+        its(:file_path) { should == 'nested/path' }
+        its(:text) { should == 'file text' } # this is a terrible test, considering the stubbing
+      end
+    end
+
+    subject { described_class.new('gem', 'gem/license/path') }
+
+    context "with a known license" do
       before do
-        subject.stub(:text).and_return('file text')
+        subject.stub(:text).and_return('a known license')
+
+        License.stub(:find_by_text).with('a known license').and_return(License.find_by_name("MIT"))
       end
 
-      its(:file_path) { should == 'nested/path' }
-      its(:text) { should == 'file text' } # this is a terrible test, considering the stubbing
-    end
-  end
-
-  subject { described_class.new('gem', 'gem/license/path') }
-
-  context "with a known license" do
-    before do
-      subject.stub(:text).and_return('a known license')
-
-      License.stub(:find_by_text).with('a known license').and_return(License.find_by_name("MIT"))
+      its(:license) { should == "MIT" }
     end
 
-    its(:license) { should == "MIT" }
-  end
+    context "with an unknown license" do
+      before do
+        subject.stub(:text).and_return('')
+      end
 
-  context "with an unknown license" do
-    before do
-      subject.stub(:text).and_return('')
+      its(:license) { should be_nil }
     end
-
-    its(:license) { should be_nil }
   end
-end
 end
