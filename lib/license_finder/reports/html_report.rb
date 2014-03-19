23f96b3a4ccb90f079fb89200c074a3d72d32073
Jacob Maine <jacob.maine@gmail.com>
2014-03-19 13:21:48 +0300

    Show whitelisted licenses in HTML; clean up visual design
    Still need to do markdown report

--- lib/license_finder/reports/html_report.rb
+++ lib/license_finder/reports/html_report.rb
@@ -1,3 +1,11 @@
 module LicenseFinder
-  class HtmlReport < FormattedReport; end
+  class HtmlReport < FormattedReport
+    def link_to_license(license)
+      if license.url && !license.url.empty?
+        %{<a href="#{license.url}">#{license.name}</a>}
+      else
+        license.name
+      end
+    end
+  end
 end
