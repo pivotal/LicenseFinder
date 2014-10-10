d78dcf23e0d3f25364406ae520b5627921ff5858
Mike Mazur <mmazur@gmail.com>
2014-10-09 22:49:49 -0700

    Migration converts license names to JSON

--- db/migrate/201410031451_rename_dependency_license_name.rb
+++ db/migrate/201410031451_rename_dependency_license_name.rb
@@ -1,5 +1,6 @@
 Sequel.migration do
   up do
     rename_column :dependencies, :license_name, :license_names
+    run %Q{UPDATE dependencies SET license_names='["' || license_names || '"]'}
   end
 end
