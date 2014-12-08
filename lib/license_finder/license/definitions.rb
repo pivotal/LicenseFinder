952843e528a7e471f0b39b5109c07231d8501398
Jacob Maine <jacob.maine@gmail.com>
2014-12-07 16:51:38 -0800

    Fix JRuby cucumber tests
    They were failing because JRuby uses gems with different licenses

--- lib/license_finder/license/definitions.rb
+++ lib/license_finder/license/definitions.rb
@@ -40,7 +40,17 @@ module LicenseFinder
         License.new(
           short_name:  "Apache2",
           pretty_name: "Apache 2.0",
-          other_names: ["Apache-2.0", "Apache Software License", "Apache License 2.0", "Apache License Version 2.0", "Apache Public License 2.0", "The Apache Software License, Version 2.0", "Apache 2", "Apache License", "Apache License, Version 2.0"],
+          other_names: [
+            "Apache-2.0",
+            "Apache Software License",
+            "Apache License 2.0",
+            "Apache License Version 2.0",
+            "Apache Public License 2.0",
+            "The Apache Software License, Version 2.0",
+            "Apache 2",
+            "Apache License",
+            "Apache License, Version 2.0"
+          ],
           url:         "http://www.apache.org/licenses/LICENSE-2.0.txt"
         )
       end
