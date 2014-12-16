8f6c83eab958562cbef3e2e4f67a2c3fce1eec8a
Jacob Maine <jacob.maine@gmail.com>
2014-12-15 21:03:31 -0800

    Stop modifying sqlite db

--- lib/license_finder/dependency_manager.rb
+++ lib/license_finder/dependency_manager.rb
@@ -13,42 +13,24 @@ module LicenseFinder
       @decisions ||= Decisions.saved!
     end
 
-    def sync_with_package_managers options={}
-      modifying {
-        current_dependencies = PackageSaver.save_all(current_packages)
-
-        Dependency.added_automatically.obsolete(current_dependencies).each(&:destroy)
-      }
-    end
-
     def manually_add(license, name, version)
-      raise Error.new("#{name} dependency already exists") unless Dependency.where(name: name).empty?
-
-      @decisions = decisions.
-        add_package(name, version).
-        license(name, license)
-
-      modifying {
-        dependency = Dependency.new(added_manually: true, name: name, version: version)
-        dependency.licenses = [License.find_by_name(license)].to_set
-        dependency.save
-      }
+      modifying do
+        @decisions = decisions.
+          add_package(name, version).
+          license(name, license)
+      end
     end
 
     def manually_remove(name)
-      @decisions = decisions.remove_package(name)
-
-      modifying { find_by_name(name, Dependency.added_manually).destroy }
-    end
-
-    def added_manually
-      Dependency.added_manually
+      modifying do
+        @decisions = decisions.remove_package(name)
+      end
     end
 
     def license!(name, license_name)
-      @decisions = decisions.license(name, license_name)
-      license = License.find_by_name(license_name)
-      modifying { find_by_name(name).set_license_manually!(license) }
+      modifying do
+        @decisions = decisions.license(name, license_name)
+      end
     end
 
     def approve!(name, approver = nil, notes = nil)
@@ -57,8 +39,9 @@ module LicenseFinder
         why: notes,
         when: Time.now.getutc
       }
-      @decisions = decisions.approve(name, txn)
-      modifying { find_by_name(name).approve!(approver, notes)  }
+      modifying do
+        @decisions = decisions.approve(name, txn)
+      end
     end
 
     def unapproved
@@ -75,29 +58,11 @@ module LicenseFinder
     end
 
     def modifying
-      checksum_before = checksum
-      result = DB.transaction { yield }
+      yield
       decisions.save!
-      checksum_after = checksum
-
-      database_changed = checksum_before != checksum_after
-
-      if database_changed || reports_do_not_exist || reports_are_stale
-        Reporter.write_reports(acknowledged)
-      end
-
-      result
     end
 
-    private # not really private, but it looks like it is!
-
-    def reports_do_not_exist
-      !(LicenseFinder.config.artifacts.html_file.exist?)
-    end
-
-    def reports_are_stale
-      LicenseFinder.config.last_modified > LicenseFinder.config.artifacts.last_refreshed
-    end
+    private
 
     def current_packages
       package_managers.
@@ -109,19 +74,6 @@ module LicenseFinder
 
     def package_managers
       [Bundler, NPM, Pip, Bower, Maven, Gradle, CocoaPods]
-    end
-
-    def find_by_name(name, scope = Dependency)
-      dep = scope.first(name: name)
-      raise Error.new("could not find dependency named #{name}") unless dep
-      dep
-    end
-
-    def checksum
-      database_file = LicenseFinder.config.artifacts.database_file
-      if database_file.exist?
-        Digest::SHA2.file(database_file).hexdigest
-      end
     end
 
     def with_decided_license(package)
--- spec/lib/license_finder/dependency_manager_spec.rb
+++ spec/lib/license_finder/dependency_manager_spec.rb
@@ -15,55 +15,7 @@ module LicenseFinder
       allow(Reporter).to receive(:write_reports)
     end
 
-    describe "#sync" do
-      let(:gem1) { ManualPackage.new("current dependency 1") }
-      let(:gem2) { ManualPackage.new("current dependency 2") }
-      let!(:bundler) { Bundler.new }
-
-      before { allow(Bundler).to receive(:new) { bundler } }
-
-      it "destroys every dependency except for the ones Bundler reports as 'current' or are marked as 'added_manually'" do
-        cur1 = Dependency.create(name: "current dependency 1")
-        cur2 = Dependency.create(name: "current dependency 2")
-        man1 = Dependency.create(name: "manual dependency", added_manually: true)
-        Dependency.create(name: "old dependency 1")
-        Dependency.create(name: "old dependency 2")
-
-        current_packages = [gem1, gem2]
-        allow(bundler).to receive(:current_packages) { current_packages }
-        expect(PackageSaver).to receive(:save_all).with(current_packages).and_return([cur1, cur2])
-
-        dependency_manager.sync_with_package_managers
-        expect(Dependency.all.map(&:name)).to match_array([cur1, cur2, man1].map(&:name))
-      end
-    end
-
     describe ".manually_add" do
-      it "should add a Dependency" do
-        expect do
-          dependency_manager.manually_add("MIT", "js_dep", "0.0.0")
-        end.to change(Dependency, :count).by(1)
-      end
-
-      it "should mark the dependency as manual" do
-        expect(dependency_manager.manually_add("MIT", "js_dep", "0.0.0"))
-          .to be_added_manually
-      end
-
-      it "should set the appropriate values" do
-        dep = dependency_manager.manually_add("GPL", "js_dep", "0.0.0")
-        expect(dep.name).to eq("js_dep")
-        expect(dep.version).to eq("0.0.0")
-        expect(dep.licenses.first.name).to eq("GPL")
-        expect(dep).not_to be_approved
-      end
-
-      it "should complain if the dependency already exists" do
-        Dependency.create(name: "current dependency 1")
-        expect { dependency_manager.manually_add("GPL", "current dependency 1", "0.0.0") }
-          .to raise_error(Error)
-      end
-
       it "should add decisions" do
         dependency_manager.manually_add("MIT", "js_dep", "0.0.0")
         decisions = dependency_manager.decisions
@@ -73,22 +25,6 @@ module LicenseFinder
     end
 
     describe ".manually_remove" do
-      it "should remove a manually managed Dependency" do
-        dependency_manager.manually_add("GPL", "a manually managed dep", nil)
-        expect do
-          dependency_manager.manually_remove("a manually managed dep")
-        end.to change(Dependency, :count).by(-1)
-      end
-
-      it "should not remove a bundler Dependency" do
-        Dependency.create(name: "a bundler dep")
-        expect do
-          expect do
-            dependency_manager.manually_remove("a bundler dep")
-          end.to raise_error(Error)
-        end.to_not change(Dependency, :count)
-      end
-
       context "with a previous decision to manually add a dependency" do
         let(:decisions) do
           result = Decisions.new.add_package("a manually managed dep", nil)
@@ -106,28 +42,6 @@ module LicenseFinder
     end
 
     describe ".approve!" do
-      it "approves the dependency" do
-        dep = Dependency.named("current dependency")
-        dep.licenses = [License.find_by_name('not approved')].to_set
-        dep.save
-        expect(dep.reload).not_to be_approved
-        dependency_manager.approve!("current dependency")
-        expect(dep.reload).to be_approved
-      end
-
-      it "optionally adds approver and approval notes" do
-        dep = Dependency.named("current dependency")
-        dependency_manager.approve!("current dependency", "Julian", "We really need this")
-        approval = dep.reload.manual_approval
-        expect(approval.approver).to eq "Julian"
-        expect(approval.notes).to eq "We really need this"
-      end
-
-      it "should raise an error if it can't find the dependency" do
-        expect { dependency_manager.approve!("non-existent dependency") }
-          .to raise_error(Error)
-      end
-
       it "should add decisions" do
         dep = Dependency.named("current dependency")
         dependency_manager.approve!("current dependency")
@@ -138,17 +52,6 @@ module LicenseFinder
 
     describe ".license!" do
       let(:dependency) { double(:dependency) }
-
-      it "adds a license for the dependency" do
-        allow(dependency_manager).to receive(:find_by_name).with("dependency").and_return(dependency)
-        expect(dependency).to receive(:set_license_manually!).with(License.find_by_name "MIT")
-        dependency_manager.license!("dependency", "MIT")
-      end
-
-      it "should raise an error if it can't find the dependency" do
-        expect { dependency_manager.license!("non-existent dependency", "a license") }
-          .to raise_error(Error)
-      end
 
       it "should add decisions" do
         dep = Dependency.named("dependency")
@@ -233,84 +136,6 @@ module LicenseFinder
           [grandparent].to_set,
           [parent].to_set
         ])
-      end
-    end
-
-    describe ".modifying" do
-      let(:file_exists) { double(:file, :exist? => true) }
-      let(:file_does_not_exist) { double(:file, :exist? => false) }
-
-      context "when the database doesn't exist" do
-        before do
-          allow(config.artifacts).to receive(:database_file).and_return(file_does_not_exist)
-        end
-
-        it "writes reports" do
-          expect(Reporter).to receive(:write_reports)
-          dependency_manager.modifying {}
-        end
-      end
-
-      context "when the database exists" do
-        before do
-          allow(config.artifacts).to receive(:database_file).and_return(file_exists)
-        end
-
-        context "when the database has changed" do
-          before do
-            i = 0
-            allow(Digest::SHA2).to receive_message_chain(:file, :hexdigest) { i += 1 }
-          end
-
-          it "writes reports" do
-            expect(Reporter).to receive(:write_reports)
-            dependency_manager.modifying {}
-          end
-        end
-
-        context "when the database has not changed" do
-          before do
-            allow(Digest::SHA2).to receive_message_chain(:file, :hexdigest) { 5 }
-            allow(config).to receive(:last_modified) { config_last_update }
-            allow(config.artifacts).to receive(:last_refreshed) { artifacts_last_update }
-          end
-
-          context "and the reports do not exist" do
-            before do
-              allow(config.artifacts).to receive(:html_file).and_return(file_does_not_exist)
-            end
-
-            it "writes reports" do
-              expect(Reporter).to receive(:write_reports)
-              dependency_manager.modifying {}
-            end
-          end
-
-          context "and the reports exist" do
-            before do
-              allow(config.artifacts).to receive(:html_file).and_return(file_exists)
-            end
-
-            context "and configs are newer than the reports" do
-              let(:config_last_update) { 4 }
-              let(:artifacts_last_update) { 1 }
-              it "writes reports" do
-                expect(Reporter).to receive(:write_reports)
-                dependency_manager.modifying {}
-              end
-            end
-
-            context "and configs are older than the reports" do
-              let(:config_last_update) { 4 }
-              let(:artifacts_last_update) { 6 }
-
-              it "does not write reports" do
-                expect(Reporter).not_to receive(:write_reports)
-                dependency_manager.modifying {}
-              end
-            end
-          end
-        end
       end
     end
   end
