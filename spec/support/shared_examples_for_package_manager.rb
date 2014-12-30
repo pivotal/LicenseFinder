edab032051c696de7cf5ac6ee3107453fd979058
Jacob Maine <jacob.maine@gmail.com>
2014-12-29 17:38:22 -0800

    Help creators of PackageManagers integrate their work into rest of system

--- spec/support/shared_examples_for_package_manager.rb
+++ spec/support/shared_examples_for_package_manager.rb
@@ -1,6 +1,7 @@
 module LicenseFinder
   shared_examples "a PackageManager" do
     it { expect(described_class.ancestors).to include PackageManager }
+    it { expect(PackageManager.package_managers).to include described_class }
 
     context "logging" do
       it "logs when it checks for active-ness" do
