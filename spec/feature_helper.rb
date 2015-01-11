e864f748d65f676336d36c482f4986623f2b5638
Jacob Maine <jacob.maine@gmail.com>
2015-01-11 13:51:52 -0800

    Run integration tests outside of a bundler environment

--- spec/feature_helper.rb
+++ spec/feature_helper.rb
@@ -1,7 +1,8 @@
-require 'rubygems'
-require 'bundler/setup'
-
-require 'license_finder'
+# These are integration tests, so reaching directly into LicenseFinder is forbidden
+# DO NOT:
+# require 'rubygems'
+# require 'bundler/setup'
+# require 'license_finder'
 
 require './features/support/testing_dsl'
 
