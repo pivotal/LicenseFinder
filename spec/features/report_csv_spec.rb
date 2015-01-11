c621bdb95dad80778439b6599ee3854d22280202
Jacob Maine <jacob.maine@gmail.com>
2015-01-10 17:39:55 -0800

    Convert csv report integration test to RSpec

---
+++ spec/features/report_csv_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+require './features/step_definitions/testing_dsl'
+
+describe "CSV report" do
+  # As a non-technical application product owner
+  # I want license finder to generate an easy-to-understand text report
+  # So that I can quickly review my application dependencies and licenses
+
+  let(:user) { LicenseFinder::TestingDSL::User.new }
+
+  specify "shows dependency data in CSV form" do
+    user.create_empty_project
+    user.execute_command 'license_finder dependencies add info_gem BSD 1.1.1'
+
+    user.execute_command('license_finder report --format csv --columns approved name version licenses')
+    expect(user).to be_seeing "Not approved,info_gem,1.1.1,BSD"
+  end
+end
