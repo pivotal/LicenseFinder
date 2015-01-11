8273183ffc9b8c36dc43a1315096d1b098003ca0
Jacob Maine <jacob.maine@gmail.com>
2015-01-10 19:03:37 -0800

    Convert Bower integration test to RSpec

---
+++ spec/features/bower_dependencies_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+require './features/step_definitions/testing_dsl'
+
+describe "Bower Dependencies" do
+  # As a JS developer
+  # I want to be able to manage Bower dependencies
+
+  let(:user) { LicenseFinder::TestingDSL::User.new }
+
+  specify "are shown in reports" do
+    user.create_bower_app
+    user.run_license_finder
+    expect(user).to be_seeing_line "gmaps, 0.2.30, MIT"
+  end
+end
