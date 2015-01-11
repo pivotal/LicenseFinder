d78be355e693c8958949d519f9386281ef5c8031
Jacob Maine <jacob.maine@gmail.com>
2015-01-10 19:08:54 -0800

    Convert Maven integration test to RSpec

---
+++ spec/features/maven_dependencies_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+require './features/step_definitions/testing_dsl'
+
+describe "Maven Dependencies" do
+  # As a Java developer
+  # I want to be able to manage Maven dependencies
+
+  let(:user) { LicenseFinder::TestingDSL::User.new }
+
+  specify "are shown in reports" do
+    user.create_maven_app
+    user.run_license_finder
+    expect(user).to be_seeing_line 'junit, 4.11, "Common Public License Version 1.0"'
+  end
+end
