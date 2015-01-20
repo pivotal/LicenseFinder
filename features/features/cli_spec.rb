0658ad30258813ad04b9a3abe671e0f9d7cb2d33
Jacob Maine <jacob.maine@gmail.com>
2015-01-19 22:50:00 -0800

    Missed an integration test persona

--- features/features/cli_spec.rb
+++ features/features/cli_spec.rb
@@ -5,39 +5,39 @@ describe "License Finder command line executable" do
   # I want a command-line interface
   # So that I can manage my application's dependencies and licenses
 
-  let(:user) { LicenseFinder::TestingDSL::User.new }
+  let(:developer) { LicenseFinder::TestingDSL::User.new }
 
   specify "reports unapproved dependencies" do
-    user.create_empty_project
-    user.execute_command("license_finder dependencies add unapproved_gem Whatever")
+    developer.create_empty_project
+    developer.execute_command("license_finder dependencies add unapproved_gem Whatever")
 
-    user.run_license_finder
-    expect(user).to be_receiving_exit_code(1)
-    expect(user).to be_seeing 'unapproved_gem'
+    developer.run_license_finder
+    expect(developer).to be_receiving_exit_code(1)
+    expect(developer).to be_seeing 'unapproved_gem'
   end
 
   specify "reports that all dependencies are approved" do
-    user.create_empty_project
+    developer.create_empty_project
 
-    user.run_license_finder
-    expect(user).to be_receiving_exit_code(0)
-    expect(user).to be_seeing 'All dependencies are approved for use'
+    developer.run_license_finder
+    expect(developer).to be_receiving_exit_code(0)
+    expect(developer).to be_seeing 'All dependencies are approved for use'
   end
 
   specify "shows usage and subcommand help" do
-    user.create_empty_project
+    developer.create_empty_project
 
-    user.execute_command "license_finder help"
-    expect(user).to be_seeing 'license_finder help [COMMAND]'
+    developer.execute_command "license_finder help"
+    expect(developer).to be_seeing 'license_finder help [COMMAND]'
 
-    user.execute_command "license_finder ignored_groups help add"
-    expect(user).to be_seeing 'license_finder ignored_groups add GROUP'
+    developer.execute_command "license_finder ignored_groups help add"
+    expect(developer).to be_seeing 'license_finder ignored_groups add GROUP'
   end
 
   it "reports `license_finder`'s license is MIT" do
-    user.create_ruby_app # has license_finder as a dependency
+    developer.create_ruby_app # has license_finder as a dependency
 
-    user.run_license_finder
-    expect(user).to be_seeing_something_like /license_finder.*MIT/
+    developer.run_license_finder
+    expect(developer).to be_seeing_something_like /license_finder.*MIT/
   end
 end
