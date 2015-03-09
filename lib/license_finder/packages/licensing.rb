7eedfd973babbdbab822fac1b2432951eb43f4d8
Jacob Maine <jacob.maine@gmail.com>
2015-03-09 08:30:59 -0700

    Make activation algorithm easier to read

--- lib/license_finder/packages/licensing.rb
+++ lib/license_finder/packages/licensing.rb
@@ -7,30 +7,26 @@ module LicenseFinder
     # among the various sources of licenses we know about.  In order of
     # priority, licenses come from decisions, package specs, or package files.
     def activations
-      afd = activations_from_decisions
-      return afd if afd.any?
-
-      afs = activations_from_spec
-      return afs if afs.any?
-
-      aff = activations_from_files
-      return aff if aff.any?
-
-      [default_activation]
+      case
+      when activations_from_decisions.any? then activations_from_decisions
+      when activations_from_spec.any?      then activations_from_spec
+      when activations_from_files.any?     then activations_from_files
+      else                                      [default_activation]
+      end
     end
 
     def activations_from_decisions
-      decided_licenses
+      @afd ||= decided_licenses
         .map { |license| Activation::FromDecision.new(package, license) }
     end
 
     def activations_from_spec
-      licenses_from_spec
+      @afs ||= licenses_from_spec
         .map { |license| Activation::FromSpec.new(package, license) }
     end
 
     def activations_from_files
-      license_files
+      @aff ||= license_files
         .group_by(&:license)
         .map { |license, files| Activation::FromFiles.new(package, license, files) }
     end
