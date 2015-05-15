26dc2bd10c6e34417e44407cc771b420174e3287
Luke Winikates <luke.winikates@gmail.com>
2015-05-14 21:11:52 -0700

    implement nuget assembly discovery

--- lib/license_finder/package_managers/nuget.rb
+++ lib/license_finder/package_managers/nuget.rb
@@ -3,6 +3,12 @@ module LicenseFinder
     def package_path
       project_path.join('packages')
     end
+
+    def assemblies
+      Dir[project_path.join("**", "packages.config")].map do |d|
+        Pathname.new(d).dirname.basename.to_s
+      end
+    end
   end
 end
 
--- spec/lib/license_finder/package_managers/nuget_spec.rb
+++ spec/lib/license_finder/package_managers/nuget_spec.rb
@@ -1,7 +1,29 @@
 require 'spec_helper'
+require 'fakefs/spec_helpers'
 
 module LicenseFinder
   describe Nuget do
+
     it_behaves_like "a PackageManager"
+
+    describe "#assemblies" do
+      include FakeFS::SpecHelpers
+
+      before do
+        FileUtils.mkdir_p "app/packages"
+        FileUtils.mkdir_p "app/Assembly1/"
+        FileUtils.mkdir_p "app/Assembly1.Tests/"
+        FileUtils.mkdir_p "app/Assembly2/"
+        FileUtils.touch "app/Assembly1/packages.config"
+        FileUtils.touch "app/Assembly1.Tests/packages.config"
+        FileUtils.touch "app/Assembly2/packages.config"
+      end
+
+      it "finds dependencies all subdirectories containing a packages.config" do
+        nuget = Nuget.new project_path: Pathname.new("app")
+        expect(nuget.assemblies).to match_array ['Assembly1', 'Assembly1.Tests', 'Assembly2']
+      end
+    end
   end
 end
+
