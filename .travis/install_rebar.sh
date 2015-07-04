0013cdb1d5cedb10faea3281158fbdc98662c300
Jacob Maine <jacob.maine@gmail.com>
2015-07-03 19:40:27 -0700

    debug travis

--- .travis/install_rebar.sh
+++ .travis/install_rebar.sh
@@ -2,14 +2,11 @@
 set -e
 set -x
 
-pushd /tmp
+pushd ~
 
 git clone --depth 1 git://github.com/rebar/rebar.git
 cd rebar
 ./bootstrap
-
-mkdir -p ~/rebar
-mv rebar ~/rebar/
 
 erl -version
 PATH=$HOME/rebar:$PATH rebar --version
