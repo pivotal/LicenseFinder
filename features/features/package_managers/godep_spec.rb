d7fbf0bacccadbe118ba14729f883d121d6bd332
Erich Douglass <erich.douglass@gmail.com>
2015-07-07 14:07:33 -0700

    Fixed feature tests to reflect godep output changes
    Signed-off-by: Heather Farley <hrfarley@gmail.com>

--- features/features/package_managers/godep_spec.rb
+++ features/features/package_managers/godep_spec.rb
@@ -12,7 +12,7 @@ describe 'Godep Dependencies' do
     ENV['GOPATH'] = "#{project.project_dir}/gopath"
 
     go_developer.run_license_finder('gopath/src/github.com/pivotal/foo')
-    expect(go_developer).to be_seeing_line 'ginkgo, d981d36, MIT'
-    expect(go_developer).to be_seeing_line 'gomega, d6c945f, MIT'
+    expect(go_developer).to be_seeing_line 'github.com/onsi/ginkgo, d981d36, MIT'
+    expect(go_developer).to be_seeing_line 'github.com/onsi/gomega, d6c945f, MIT'
   end
 end
