4326c3435332d06b410a2672d28d1343c4059fae
Amin Jamali <ajamali@pivotal.io>
2016-02-09 16:52:26 -0500

    removes an unused method from GoPackage
    Signed-off-by: John Shahid <jshahid@pivotal.io>

--- lib/license_finder/package_managers/go_package.rb
+++ lib/license_finder/package_managers/go_package.rb
@@ -1,9 +1,5 @@
 module LicenseFinder
   class GoPackage < Package
-    def self.from_workspace(name, path)
-      LicenseFinder::Package.new(name, 'unknown', {install_path: install_path(path)})
-    end
-
     def self.from_dependency(hash, prefix,full_version)
       name = hash['ImportPath']
       install_path = hash['InstallPath']
