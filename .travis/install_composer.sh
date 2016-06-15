06061c341f6a77eb5ced80883552738d08486a63
Simon Yousoufov <syousoufov@fuzzproductions.com>
2016-06-15 00:08:33 -0400

    Require sudo for travis CI, update install_composer script to also install PHP

--- .travis/install_composer.sh
+++ .travis/install_composer.sh
@@ -2,12 +2,20 @@
 set -e
 set -x
 
-pushd /tmp
+# Install PHP
+sudo apt-get install python-software-properties
+sudo add-apt-repository -y ppa:ondrej/php
+sudo apt-get update
+sudo apt-get install -y php5.6
+
+# Install and set up composer
+mkdir -p $HOME/composer
+pushd $HOME/composer
 
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 php -r "if (hash_file('SHA384', 'composer-setup.php') === '070854512ef404f16bac87071a6db9fd9721da1684cd4589b1196c3faf71b9a2682e2311b36a5079825e155ac7ce150d') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
 php composer-setup.php
 php -r "unlink('composer-setup.php');"
-mv composer.phar /usr/local/bin/composer
+mv $HOME/composer/composer.phar $HOME/composer/composer
 
 popd
