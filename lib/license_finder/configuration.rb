pivotal/LicenseFinder

commit f781fc5d0e3a86d685934e1bece97606127229ac

conditionally invoke package manager
only when the package management command exists on the user's path.

lib/license_finder/package_managers/bower.rb
@@ -8,6 +8,10 @@ module LicenseFinder
       end
     end
 
+    def self.package_management_command
+      "bower"
+    end
+
     private
 
     def bower_output

lib/license_finder/package_managers/gradle.rb
@@ -26,6 +26,10 @@ module LicenseFinder
       packages.uniq
     end
 
+    def self.package_management_command
+      "gradle"
+    end
+
     private
 
     def package_path

lib/license_finder/package_managers/maven.rb
@@ -20,6 +20,10 @@ module LicenseFinder
       end
     end
 
+    def self.package_management_command
+      "mvn"
+    end
+
     private
 
     def license_report

lib/license_finder/package_managers/npm.rb
@@ -22,6 +22,10 @@ module LicenseFinder
       packages.values
     end
 
+    def self.package_management_command
+      "npm"
+    end
+
     private
 
     def direct_dependencies

lib/license_finder/package_managers/rebar.rb
@@ -18,6 +18,10 @@ module LicenseFinder
       end
     end
 
+    def self.package_management_command
+      "rebar"
+    end
+
     private
 
     def rebar_ouput

Rakefile
@@ -26,31 +26,10 @@ end
 
 desc "Check for non-Ruby development dependencies."
 task :check_dependencies do
-  dependencies = {
-    "mvn" => "Maven",
-    "npm" => "NPM",
-    "pip" => "Pip",
-    "gradle" => "Gradle",
-    "bower" => "Bower",
-    "rebar" => "Rebar",
-    "godep" => "Go"
-  }
-  dependencies["pod"] = "Cocoapods" if LicenseFinder::Platform.darwin?
+  require './lib/license_finder'
   satisfied = true
-  dependencies.each do |dependency, description|
-    printf "checking dev dependency for #{description} ... "
-    if LicenseFinder::Platform.windows?
-      `where #{dependency} 2>NUL`
-    else
-      `which #{dependency} 2>/dev/null`
-    end
-    status = $?
-    if status.success?
-      puts "OK"
-    else
-      puts "missing `#{dependency}`"
-      satisfied = false
-    end
+  LicenseFinder::PackageManager.package_managers.each do |package_manager|
+    satisfied = false unless package_manager.installed?(LicenseFinder::Logger.new(debug:true))
   end
   exit 1 unless satisfied
 end

lib/license_finder/logger.rb
@@ -12,8 +12,22 @@ module LicenseFinder
     end
 
     class Base
+      def installed package_manager, is_installed
+        if String === is_installed
+          log package_manager, is_installed
+        elsif is_installed
+          log package_manager, Logger.green("is installed")
+        else
+          log package_manager, Logger.red("is not installed")
+        end
+      end
+
       def active package_manager, is_active
-        log package_manager, sprintf("%s active", (is_active ? "is" : "not"))
+        if is_active
+          log package_manager, Logger.green("is active")
+        else
+          log package_manager, "is not active"
+        end
       end
 
       def package package_manager, package
@@ -42,6 +56,18 @@ module LicenseFinder
       def log prefix, string
         raise NotImplementedError, "#log must be implemented"
       end
+    end
+
+    def self.green string
+      colorize 32, string
+    end
+
+    def self.red string
+      colorize 31, string
+    end
+
+    def self.colorize color_code, string
+      "\e[#{color_code}m#{string}\e[0m"
     end
 
     class Quiet < Base

lib/license_finder/package_manager.rb
@@ -23,14 +23,33 @@ module LicenseFinder
       active_package_managers.flat_map(&:current_packages_with_relations)
     end
 
+    def self.installed?(logger=Core.default_logger)
+      if package_management_command.nil?
+        logger.installed self, "no command defined" # TODO comment me out
+        return true
+      end
+
+      if command_exists?(package_management_command)
+        logger.installed self, true
+        return true
+      end
+
+      logger.installed self, false
+      return false
+    end
+
+    def self.package_management_command
+      nil
+    end
+
     def initialize options={}
       @logger       = options[:logger] || Core.default_logger
       @project_path = options[:project_path]
     end
 
     def active?
-      package_path.exist?
-        .tap { |is_active| logger.active self.class, is_active }
+      self.class.installed?(logger) &&
+        package_path.exist?.tap { |is_active| logger.active self.class, is_active }
     end
 
     def capture(command)
@@ -46,6 +65,16 @@ module LicenseFinder
         end
       end
       packages
+    end
+
+    def self.command_exists? command
+      if LicenseFinder::Platform.windows?
+        `where #{command} 2>NUL`
+      else
+        `which #{command} 2>/dev/null`
+      end
+      status = $?
+      return status.success?
     end
 
     private

commit 8fb77bd685fd1d3680cf7d8675a9e1baced979b1

Make the package managers the canonical place for their commands
Do not implement fallbacks for commands in the configuration *and* package
managers, but only in the latter for clarity. Remove the Gradle test that
checks for a default command in the configuration.

Also, do not repeat the package command as part of the full command line,
but simply insert the package management command.

As a side effect this fixes #222 which is about activating the Gradle
package manager if the "gradle" executable is not in PATH but the Gradle
wrapper is in the project path, which was broken by commit f781fc5.

lib/license_finder/configuration.rb
@@ -22,17 +22,7 @@ module LicenseFinder
     end
 
     def gradle_command
-      get(:gradle_command) || (
-        if Platform.windows?
-          wrapper = 'gradlew.bat'
-          gradle = 'gradle.bat'
-        else
-          wrapper = './gradlew'
-          gradle = 'gradle'
-        end
-
-        File.exist?(File.join(project_path, wrapper)) ? wrapper : gradle
-      )
+      get(:gradle_command)
     end
 
     def go_full_version
@@ -44,7 +34,7 @@ module LicenseFinder
     end
 
     def rebar_command
-      get(:rebar_command) || 'rebar'
+      get(:rebar_command)
     end
 
     def rebar_deps_dir

lib/license_finder/package_managers/bower.rb
@@ -15,7 +15,7 @@ module LicenseFinder
     private
 
     def bower_output
-      command = 'bower list --json -l action'
+      command = "#{Bower::package_management_command} list --json -l action"
       output, success = Dir.chdir(project_path) { capture(command) }
       raise "Command '#{command}' failed to execute: #{output}" unless success
 

lib/license_finder/package_managers/gradle.rb
@@ -6,7 +6,7 @@ module LicenseFinder
   class Gradle < PackageManager
     def initialize(options={})
       super
-      @command = options[:gradle_command] || 'gradle'
+      @command = options[:gradle_command] || package_management_command
       @include_groups = options[:gradle_include_groups]
     end
 
@@ -28,8 +28,16 @@ module LicenseFinder
       end
     end
 
-    def self.package_management_command
-      "gradle"
+    def package_management_command
+        if Platform.windows?
+          wrapper = 'gradlew.bat'
+          gradle = 'gradle.bat'
+        else
+          wrapper = './gradlew'
+          gradle = 'gradle'
+        end
+
+        File.exist?(File.join(project_path, wrapper)) ? wrapper : gradle
     end
 
     private

lib/license_finder/package_managers/maven.rb
@@ -3,7 +3,7 @@ require "xmlsimple"
 module LicenseFinder
   class Maven < PackageManager
     def current_packages
-      command = 'mvn license:download-licenses'
+      command = "#{Maven::package_management_command} license:download-licenses"
       output, success = Dir.chdir(project_path) { capture(command) }
       raise "Command '#{command}' failed to execute: #{output}" unless success
 

lib/license_finder/package_managers/npm.rb
@@ -51,7 +51,7 @@ module LicenseFinder
     end
 
     def npm_json
-      command = 'npm list --json --long'
+      command = "#{NPM::package_management_command} list --json --long"
       output, success = Dir.chdir(project_path) { capture(command) }
 
       if success

lib/license_finder/package_managers/rebar.rb
@@ -2,7 +2,7 @@ module LicenseFinder
   class Rebar < PackageManager
     def initialize(options={})
       super
-      @command = options[:rebar_command] || "rebar"
+      @command = options[:rebar_command] || Rebar::package_management_command
       @deps_path = Pathname(options[:rebar_deps_dir] || "deps")
     end
 

spec/lib/license_finder/configuration_spec.rb
@@ -47,14 +47,6 @@ module LicenseFinder
         )
         expect(subject.gradle_command).to eq "secondary"
       end
-
-      it "has default" do
-        subject = described_class.new(
-          {gradle_command: nil},
-          {"gradle_command" => nil}
-        )
-        expect(subject.gradle_command).to eq "gradle"
-      end
     end
 
     describe "decisions_file" do