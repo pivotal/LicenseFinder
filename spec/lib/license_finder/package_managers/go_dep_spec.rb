f59319f508ada9f822ec02c2eb3ad17ae799df7e
Sebastian Schuberth <sschuberth@gmail.com>
2016-10-27 09:10:05 +0200

    go_dep_spec: Convert tabs to spaces

--- spec/lib/license_finder/package_managers/go_dep_spec.rb
+++ spec/lib/license_finder/package_managers/go_dep_spec.rb
@@ -73,21 +73,21 @@ module LicenseFinder
       context 'when there are duplicate dependencies' do
         let(:content) do
           '{
-	           "ImportPath": "github.com/foo/bar",
-	           "GoVersion": "go1.3",
-	           "Deps": [
-	           	{
-	           		"ImportPath": "github.com/foo/baz/sub1",
-	           		"Rev": "28838aae6e8158e3695cf90e2f0ed2498b68ee1d"
-	           	},
-	           	{
-	           		"ImportPath": "github.com/foo/baz/sub2",
-	           		"Rev": "28838aae6e8158e3695cf90e2f0ed2498b68ee1d"
-	           	},
-	           	{
-	           		"ImportPath": "github.com/foo/baz/sub3",
-	           		"Rev": "28838aae6e8158e3695cf90e2f0ed2498b68ee1d"
-	           	}
+               "ImportPath": "github.com/foo/bar",
+               "GoVersion": "go1.3",
+               "Deps": [
+                {
+                    "ImportPath": "github.com/foo/baz/sub1",
+                    "Rev": "28838aae6e8158e3695cf90e2f0ed2498b68ee1d"
+                },
+                {
+                    "ImportPath": "github.com/foo/baz/sub2",
+                    "Rev": "28838aae6e8158e3695cf90e2f0ed2498b68ee1d"
+                },
+                {
+                    "ImportPath": "github.com/foo/baz/sub3",
+                    "Rev": "28838aae6e8158e3695cf90e2f0ed2498b68ee1d"
+                }
             ]
           }'
         end
