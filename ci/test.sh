e8f4ec0fcde0aa11f5cc3ea79afea38fe7af391e
Dmitriy Dubson and Joe Moore <pair+ddubson+jmoore@pivotal.io>
2017-01-13 10:16:28 -0500

    Enables running of full test suite on Concourse.

--- ci/test.sh
+++ ci/test.sh
@@ -3,8 +3,17 @@
 set -e -x
 
 pushd LicenseFinder
+  chmod +x ci/install_*.sh
+  ./ci/install_rebar.sh
+  ./ci/install_bower.sh
+  ./ci/install_gradle.sh
+  ./ci/install_godep.sh
+  export PATH=$PATH:$HOME/gradle/bin:$HOME/rebar:$HOME/go/bin GOPATH=$HOME/go
+
   gem install bundler
   bundle install
-  ls -al
-  rspec spec/lib/license_finder/license_spec.rb
+
+  bundle exec rake install
+  bundle exec rake spec
+  bundle exec rake features
 popd
