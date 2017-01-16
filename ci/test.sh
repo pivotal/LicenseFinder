68ed76dce7c6ddaec1d7bdde96349f7263ffb532
Dmitriy Dubson and Joe Moore <pair+ddubson+jmoore@pivotal.io>
2017-01-16 12:31:00 -0500

    Reverted test.sh to include all runtimes.

--- ci/test.sh
+++ ci/test.sh
@@ -9,13 +9,13 @@ pushd LicenseFinder
   ruby --version
 
   chmod +x ci/install_*.sh
-  # ./ci/install_rebar.sh
+  ./ci/install_rebar.sh
   ./ci/install_bower.sh
-  # ./ci/install_gradle.sh
-  # ./ci/install_godep.sh
+  ./ci/install_gradle.sh
+  ./ci/install_godep.sh
   export PATH=$PATH:$HOME/gradle/bin:$HOME/rebar:$HOME/go/bin GOPATH=$HOME/go
   export RUBYOPT='-E utf-8'
-  
+
   gem update --system
   gem install bundler
   bundle install
@@ -23,10 +23,7 @@ pushd LicenseFinder
   ruby --version
   bundler --version
 
-  # bundle exec rake install
-  # bundle exec rake spec
-  # bundle exec rake features
-
-  bundle exec rspec ./spec/lib/license_finder/package_managers/npm_spec.rb:149 # LicenseFinder::NPM.current_packages npm recursive dependency edge case - GH#211 .current_packages correctly navigates the dependencies tree and pulls out valid information
-
+  bundle exec rake install
+  bundle exec rake spec
+  bundle exec rake features
 popd
