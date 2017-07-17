0e6d67c824cc5ab8049c4471e11073fb0b19e740
Ryan Collins <rcollins@pivotal.io>
2017-07-17 17:45:04 -0400

    Add support for idiosyncratic (outdated) package.json format
    [Finish #148924915]
    
    Signed-off-by: Jamie Klassen <cklassen@pivotal.io>

--- lib/license_finder/package_managers/npm.rb
+++ lib/license_finder/package_managers/npm.rb
@@ -1,96 +1,52 @@
-require 'yajl'
+require 'json'
 require 'tempfile'
 
 module LicenseFinder
   class NPM < PackageManager
-    DEPENDENCY_GROUPS = ["dependencies", "devDependencies"]
-
     def current_packages
-      top_level_deps = npm_json['dependencies']&.values || []
-      package_json = JSON.parse(File.read(package_path), :max_nesting => false)
-      top_level_deps.each do |dep|
-        dep['groups'] = DEPENDENCY_GROUPS.select do |group|
-          package_json[group]&.keys&.include? dep['name']
-        end
-      end
-      dependency_matches = flatten(top_level_deps)
-      dependency_matches = dependency_matches.group_by {|dep| [dep['name'], dep['version']]}
-      dependency_matches.map {|id, dep_matches| construct_npm_package(dep_matches, id)}
+      NpmPackage.packages_from_json(npm_json, package_path)
     end
 
     private
 
     def self.package_management_command
-      "npm"
+      'npm'
     end
 
     def package_path
       project_path.join('package.json')
     end
 
-    def npm_json
+    def run_command_with_tempfile_buffer(command, &block)
       tempfile = Tempfile.new
       begin
-        command = "#{NPM::package_management_command} list --json --long > #{tempfile.path}"
-        output, success = Dir.chdir(project_path) { capture(command) }
-
-        if success
-          json = Yajl::Parser.parse(File.open(tempfile.path))
-        else
-          json = begin
-            Yajl::Parser.parse(File.open(tempfile.path))
-          rescue Yajl::ParseError
-            nil
-          end
-          if json
-            $stderr.puts "Command '#{command}' returned an error but parsing succeeded."
-          else
-            raise "Command '#{command}' failed to execute: #{output}"
-          end
-        end
+        output, success = Dir.chdir(project_path) { capture("#{command} > #{tempfile.path}") }
+        result = block.call(File.read(tempfile.path))
       ensure
         tempfile.close
         tempfile.unlink
       end
+      [output, result, success]
+    end
+
+    def npm_json
+      command = "#{NPM::package_management_command} list --json --long"
+      output, json, success = run_command_with_tempfile_buffer(command, &:parse_json_safely)
+      unless success
+        if json
+          $stderr.puts "Command '#{command}' returned an error but parsing succeeded."
+        else
+          raise "Command '#{command}' failed to execute: #{output}"
+        end
+      end
       json
     end
+  end
 
-    def flatten(list)
-      list.inject [] {|acc, dep| acc + [dep] + flatten(dep['dependencies']&.values&.map {|inner_dep| inner_dep['groups'] = dep['groups']; inner_dep} || [])}
-    end
-
-    def licenses(dep_matches)
-      licenses_lists = dep_matches.map do |match|
-        match['licenses']
-      end
-
-      licenses_lists.reject! {|licenses| licenses.nil? || licenses == '[Circular]'}
-      licenses_lists = licenses_lists.uniq
-      case licenses_lists.count
-        when 0
-          dep_matches.map {|dep_match| dep_match['license']}.reject(&:nil?).uniq
-        when 1
-          licenses_lists.first.map {|license| license['type']}
-        else
-          raise "Varying lists of licenses provided for #{dep_matches.first['name']} (#{dep_matches.first['version']})"
-      end
-    end
-
-    def construct_npm_package(dep_matches, id)
-      name, version = id
-      dependencies = value_from_matches(dep_matches, 'dependencies')&.values&.map {|d| d['name']} || []
-      NpmPackage.new(name, version,
-                     homepage: value_from_matches(dep_matches, 'homepage'),
-                     description: value_from_matches(dep_matches, 'description'),
-                     spec_licenses: licenses(dep_matches),
-                     install_path: value_from_matches(dep_matches, 'path'),
-                     dependencies: dependencies,
-                     groups: dep_matches.map {|match| match['groups']}.flatten.uniq,
-                     logger: logger)
-    end
-
-    def value_from_matches(dep_matches, key)
-      dep_matches.map {|m| m[key]}.reject(&:nil?).uniq&.first
+  String.class_eval do
+    def parse_json_safely
+      JSON.parse(self) rescue nil
     end
   end
+
 end
--- lib/license_finder/package_managers/npm_package.rb
+++ lib/license_finder/package_managers/npm_package.rb
@@ -1,7 +1,131 @@
 module LicenseFinder
   class NpmPackage < Package
+    attr_accessor :identifier, :dependencies, :groups
+
+    def self.packages_from_json(npm_json, package_path)
+      root_package = NpmPackage.new(npm_json).decircularize
+      package_json = PackageJson.new(package_path)
+      root_package.dependencies.each { |d| d.groups = package_json.groups(d.identifier) }
+      root_package.propagate_groups
+      root_package.dependencies.map(&:flatten).flatten.uniq(&:identifier)
+    end
+
+    def initialize(npm_json, root=self)
+      @identifier = Identifier.from_hash(npm_json)
+      @root = root
+      @dependencies = npm_json.fetch('dependencies', {}).values.map { |d| NpmPackage.new(d, root) }
+      super(@identifier.name,
+            @identifier.version,
+            description: npm_json['description'],
+            homepage: npm_json['homepage'],
+            spec_licenses: Package.license_names_from_standard_spec(npm_json),
+            install_path: npm_json['path'],
+            children: @dependencies.map(&:name))
+    end
+
+    def propagate_groups
+      @dependencies.each do |child|
+        child.groups = (child.groups + @groups).uniq
+        child.propagate_groups
+      end
+      self
+    end
+
+    def decircularize
+      noncircular = @root.find_noncircular(@identifier)
+      noncircular.dependencies = @dependencies.map(&:decircularize)
+      noncircular
+    end
+
+    def flatten
+      [self] + @dependencies.map(&:flatten).flatten(1)
+    end
+
+    def find_noncircular(identifier)
+      flatten.select{ |p| p.identifier == identifier }.find(&:noncircular?)
+    end
+
+    def noncircular?
+      @licenses != ['[Circular]']
+    end
+
+    def ==(other)
+      other.is_a?(NpmPackage) && @identifier == other.identifier
+    end
+
+    def to_s
+      @identifier.to_s
+    end
+
     def package_manager
       'Npm'
+    end
+
+    class Identifier
+      attr_accessor :name, :version
+
+      def initialize(name, version)
+        @name = name
+        @version = version
+      end
+
+      def self.from_hash(hash)
+        Identifier.new(hash['name'], hash['version'])
+      end
+
+      def ==(other)
+        other.is_a?(Identifier) && @name == other.name && @version == other.version
+      end
+
+      def eql?(other)
+        self == other
+      end
+
+      def hash
+        [@name, @version].hash
+      end
+
+      def <=>(other)
+        sort_name = @name <=> other.name
+        sort_name.zero? ? @version <=> other.version : sort_name
+      end
+
+      def to_s
+        "#{@name} - #{@version}"
+      end
+
+    end
+
+    class Group
+      attr_accessor :name, :package_identifiers
+
+      def initialize(name, hash)
+        @name = name
+        @package_names = hash.keys
+      end
+
+      def include?(identifier)
+        @package_names.include? identifier.name
+      end
+
+      def to_s
+        @name
+      end
+
+    end
+
+    class PackageJson
+      DEPENDENCY_GROUPS = %w(dependencies devDependencies)
+
+      def initialize(path)
+        json = JSON.parse(File.read(path), max_nesting: false)
+        @groups = DEPENDENCY_GROUPS.map { |name| Group.new(name, json.fetch(name, {})) }
+      end
+
+      def groups(identifier)
+        @groups.select { |g| g.include? identifier }.map(&:name)
+      end
+
     end
   end
 end
