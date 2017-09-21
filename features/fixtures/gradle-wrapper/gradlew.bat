569971825a684e0d012ab69936d9a4bd456d0829
Sebastian Schuberth <sschuberth@gmail.com>
2017-09-21 16:51:05 +0200

    Improve the errors if a global Gradle installation could not be found
    Also, properly forward any given command line arguments.

--- features/fixtures/gradle-wrapper/gradlew.bat
+++ features/fixtures/gradle-wrapper/gradlew.bat
@@ -1,1 +1,9 @@
-@call gradle.bat
+@echo off
+
+where /q gradle.bat
+if %ERRORLEVEL% equ 0 (
+    call gradle.bat %*
+) else (
+    echo No Gradle installation found. Please install it from https://gradle.org/install/. >&2
+    exit /b 1
+)
