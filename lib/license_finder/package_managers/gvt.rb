96e940add3ddbec73845ac7622288139501be38b
Vlad Vassilovski <vvassilovski@pivotal.io>
2017-10-05 18:02:47 -0400

    Implement gvt support for both `project-root/vendor` and `project-root/*/vendor` file structures
    [finish #151531049]
    
    Signed-off-by: Mark Fioravanti <mfioravanti@pivotal.io>

--- lib/license_finder/package_managers/gvt.rb
+++ lib/license_finder/package_managers/gvt.rb
@@ -1,8 +1,11 @@
 module LicenseFinder
   class Gvt < PackageManager
-    SHELL_COMMAND = 'cd src && gvt list -f "{{.Importpath}} {{.Revision}} {{.Repository}}"'
     def package_path
-      project_path.join('src', 'vendor', 'manifest')
+      potential_path_list = Dir.glob project_path.join('*', 'vendor', 'manifest')
+      potential_path_list << project_path.join('vendor', 'manifest')
+      path = potential_path_list.find { |potential_path| Pathname(potential_path).exist? }
+
+      Pathname(path) unless path.nil?
     end
 
     def self.package_management_command
@@ -10,16 +13,35 @@ module LicenseFinder
     end
 
     def current_packages
-      output, success = capture(Gvt::SHELL_COMMAND)
+      split_project_path = project_path.to_s.split('/')
+      project_root_depth = split_project_path.length - 1
+
+      split_package_path = package_path.to_s.split('/')
+      vendor_dir_depth = split_package_path.index('vendor')
+      return [] if (vendor_dir_depth.nil?)
+      vendor_dir_parent_depth = vendor_dir_depth - 1
+
+      is_project_root_parent_of_vendor_dir = project_root_depth == vendor_dir_parent_depth
+      
+      if (is_project_root_parent_of_vendor_dir)
+        shell_command = 'gvt list -f "{{.Importpath}} {{.Revision}} {{.Repository}}"'
+        path = project_path.join('vendor')
+      else
+        vendor_dir_parent = split_package_path[vendor_dir_parent_depth]
+        shell_command = "cd #{vendor_dir_parent} && gvt list -f \"{{.Importpath}} {{.Revision}} {{.Repository}}\""
+        path = project_path.join(vendor_dir_parent, 'vendor')
+      end
+
+      output, success = capture(shell_command)
       return [] unless success
       package_lines = output.split("\n")
       package_lines.map do |package_line|
         import_path, revision, repo = package_line.split
         GoPackage.from_dependency({
-                                   'ImportPath' => import_path,
-                                   'InstallPath' => project_path.join('src', 'vendor', import_path),
-                                   'Rev' => revision,
-                                   'Homepage' => repo
+                                      'ImportPath' => import_path,
+                                      'InstallPath' => path.join(import_path),
+                                      'Rev' => revision,
+                                      'Homepage' => repo
                                   }, nil, true)
       end
     end
