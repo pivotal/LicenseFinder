5a7041cbaf9568df48c6a31f26ce76037362719f
Daniil Kouznetsov <dkouznetsov@pivotal.io>
2018-01-18 16:19:59 -0500

    Fix script directory issue
    Signed-off-by: Shane Lattanzio <slattanzio@pivotal.io>

--- ci/scripts/bump-patch-version.sh
+++ ci/scripts/bump-patch-version.sh
@@ -16,10 +16,11 @@ NEW_VERSION="$(cut -d'.' -f1,2 <<<"$VERSION").$NEW_PATCH"
 
 sed -i.bak "s/$VERSION/$NEW_VERSION/g" "$VERSION_FILE"
 
+cd lf-git-changed
 git config --global user.email $GIT_EMAIL
 git config --global user.name $GIT_USERNAME
 
-git add $VERSION_FILE
+git add "lib/license_finder/version.rb"
 git commit -m "Update patch version to: $NEW_VERSION"
 
 exit 0
