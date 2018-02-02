449d59b8a983a5c11bc8d33bd8c63d6c41b1c5bc
Ryan Collins <rcollins@pivotal.io>
2018-02-02 15:41:22 -0500

    Add git creds to RubyGems pushscript
    Signed-off-by: Shane Lattanzio <slattanzio@pivotal.io>

--- ci/scripts/pushscript.sh
+++ ci/scripts/pushscript.sh
@@ -7,7 +7,11 @@ cd lf-git
 build_version=$(ruby -r ./lib/license_finder/version.rb -e "puts LicenseFinder::VERSION")
 built_gem="pkg/license_finder-$build_version.gem"
 
+git config --global user.email $GIT_EMAIL
+git config --global user.name $GIT_USERNAME
+
 if [ -z "$(gem fetch license_finder -v $build_version 2>&1 | grep ERROR)" ]; then
+  echo "LicenseFinder-$build_version already exists on Rubygems"
   exit 0
 fi
 
