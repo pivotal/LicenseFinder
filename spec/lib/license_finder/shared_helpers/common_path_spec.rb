9e1071d3c92405a8605727ad1164d6581dc50533
Ryan Collins <rcollins@pivotal.io>
2018-03-21 14:54:55 -0400

    Condense gvt paths with identical shas into their common path
    [Finish #155382947, #154072994, #155830792]
    
    Signed-off-by: Vikram Yadav <vyadav@pivotal.io>

---
+++ spec/lib/license_finder/shared_helpers/common_path_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+require 'license_finder/shared_helpers/common_path'
+
+describe CommonPathHelper do
+  context 'when the GVT returns entries with same sha with common base path' do
+    let(:gvt_output_with_common_paths) do
+      %w[cloud.google.com/go/bigquery cloud.google.com/go/civil cloud.google.com/go/compute/metadata]
+    end
+    let(:gvt_output_without_common_paths) do
+      %w[cloud.google.com/go/bigquery/adsf cloud.google.com/go/civil cloud.aws.com/go/metadata]
+    end
+
+    it 'only shows the entry with common base path once' do
+      paths = CommonPathHelper.shortest_common_paths gvt_output_with_common_paths
+      expect(paths).to match_array %w[cloud.google.com/go]
+    end
+
+    it 'shows entries with same sha when they do not have a common base path' do
+      paths = CommonPathHelper.shortest_common_paths gvt_output_without_common_paths
+      expect(paths).to match_array %w[cloud.google.com/go cloud.aws.com/go/metadata]
+    end
+  end
+end
