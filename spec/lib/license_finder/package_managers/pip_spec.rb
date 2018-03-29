3901bdaf7d5203f6d4fa60e6d3835b4c2a4c148a
Shane Lattanzio <slattanzio@pivotal.io>
2018-03-29 12:03:30 -0400

    [Fixed] Added back the pip prepare commands [#156376451 finish]
    Signed-off-by: Tony Wong <twong@pivotal.io>

--- spec/lib/license_finder/package_managers/pip_spec.rb
+++ spec/lib/license_finder/package_managers/pip_spec.rb
@@ -2,8 +2,65 @@ require 'spec_helper'
 
 module LicenseFinder
   describe Pip do
-    let(:pip) { Pip.new }
+    let(:root) { '/fake-python-project' }
+    let(:pip) { Pip.new(project_path: Pathname(root)) }
     it_behaves_like 'a PackageManager'
+
+
+    let(:requirements_txt) do
+<<eos
+tox>=2.3.1,<3.0.0
+docutils>=0.10
+# botocore and the awscli packages are typically developed
+# in tandem, so we're requiring the latest develop
+# branch of botocore when working on the awscli.
+-e git://github.com/boto/botocore.git@develop#egg=botocore
+-e git://github.com/boto/s3transfer.git@develop#egg=s3transfer
+-e git://github.com/boto/jmespath.git@develop#egg=jmespath
+nose==1.3.0
+colorama>=0.2.5,<=0.3.7
+mock==1.3.0
+rsa>=3.1.2,<=3.5.0
+wheel==0.24.0
+PyYAML>=3.10,<=3.12"
+eos
+    end
+
+    let(:dependency_json) do
+      FakeFS.without do
+        fixture_from('pip.json')
+      end
+    end
+
+    describe '.prepare' do
+      include FakeFS::SpecHelpers
+      before do
+        FileUtils.mkdir_p(Dir.tmpdir)
+        FileUtils.mkdir_p(root)
+        File.write(File.join(root, 'requirements.txt'), requirements_txt)
+        user_provided_dir = File.join(root, 'user-provided')
+        @user_provided_requirements = File.join(user_provided_dir, 'requirements.txt')
+        FileUtils.mkdir_p(user_provided_dir)
+        File.write(@user_provided_requirements, requirements_txt)
+      end
+
+      it 'should call pip install with the requirements file' do
+        expect(SharedHelpers::Cmd).to receive(:run).with('pip install -r requirements.txt')
+                                          .and_return([dependency_json, '', cmd_success])
+        pip.prepare
+      end
+
+      context 'user provides a requirements file' do
+        let(:pip) { Pip.new(project_path: Pathname(root), pip_requirements_path: @user_provided_requirements) }
+
+        it 'should use the provided requirements file' do
+          expect(SharedHelpers::Cmd).to receive(:run).with("pip install -r #{@user_provided_requirements}")
+                                            .and_return([dependency_json, '', cmd_success])
+          pip.prepare
+        end
+      end
+    end
+
 
     describe '.current_packages' do
       def stub_pip(stdout)
