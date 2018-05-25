db095078579b5436321d7257882759667c7365a4
Chris MacNaughton <chris@centaurisolutions.nl>
2018-05-25 21:15:35 +0200

    fix lint

--- lib/license_finder/packages/cargo_package.rb
+++ lib/license_finder/packages/cargo_package.rb
@@ -1,7 +1,7 @@
 module LicenseFinder
   class CargoPackage < Package
     def initialize(crate, options = {})
-      crate = crate.reject { |k, v| v.nil? || v == '' }
+      crate = crate.reject { |_, v| v.nil? || v == '' }
       children = crate.fetch('dependencies', []).map { |p| p['name'] }
       licenses = crate.fetch('license', '').split('/')
       super(
