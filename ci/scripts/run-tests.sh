a5668a97e284a4678f063a2bfe9b7a0a6370e53e
Tony Wong <twong@pivotal.io>
2018-06-06 11:51:50 -0400

    run-tests.sh can be executed from any directory
    [#157916095]

--- ci/scripts/run-tests.sh
+++ ci/scripts/run-tests.sh
@@ -1,30 +1,38 @@
 #!/bin/bash -elx
+
+set -o pipefail
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+PROJECT_ROOT="$( dirname "$( dirname $DIR )" )"
+
 RUBY_VERSION_UNDER_TEST=$1
-rvm install --default $RUBY_VERSION_UNDER_TEST
-ruby --version
 
-export GOPATH=$HOME/go
-export RUBYOPT='-E utf-8'
+pushd "$PROJECT_ROOT"
 
-gem update --system
-gem install bundler
-bundle install
+  rvm install --default $RUBY_VERSION_UNDER_TEST
+  ruby --version
 
-# jruby-9 specific: requires  >= rack 2.x
-if [ "$RUBY_VERSION_UNDER_TEST" == "jruby-9.0.4.0" ]
-then
-  bundle update rack
-  apt-get -y install software-properties-common
-  add-apt-repository -y ppa:webupd8team/java
-  apt-get update
-  echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections
-  apt-get -y install oracle-java8-set-default
-fi
+  export GOPATH=$HOME/go
+  export RUBYOPT='-E utf-8'
 
+  gem update --system
+  gem install bundler
+  bundle install
 
-bundle exec rake install
-bundle exec rake spec
-bundle exec rake features
+  # jruby-9 specific: requires  >= rack 2.x
+  if [ "$RUBY_VERSION_UNDER_TEST" == "jruby-9.0.4.0" ]
+  then
+    bundle update rack
+    apt-get -y install software-properties-common
+    add-apt-repository -y ppa:webupd8team/java
+    apt-get update
+    echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections
+    apt-get -y install oracle-java8-set-default
+  fi
 
 
+  bundle exec rake install
+  bundle exec rake spec
+  bundle exec rake features
 
+popd
