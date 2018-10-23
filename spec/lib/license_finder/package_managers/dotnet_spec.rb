7e81768f2236c2d0f247e2091b2041547063221b
Catherine Davie <cdavie@pivotal.io>
2018-10-23 14:00:17 -0700

    Improved dotnet tests
    Signed-off-by: Steve Gravrock <sgravrock@pivotal.io>

--- spec/lib/license_finder/package_managers/dotnet_spec.rb
+++ spec/lib/license_finder/package_managers/dotnet_spec.rb
@@ -3,10 +3,6 @@ require 'fakefs/spec_helpers'
 require 'set'
 
 module LicenseFinder
-  def self.broken_fakefs?
-    RUBY_PLATFORM =~ /java/ || RUBY_VERSION =~ /^(1\.9|2\.0)/
-  end
-
   describe Dotnet do
     it_behaves_like 'a PackageManager'
 
@@ -89,10 +85,27 @@ module LicenseFinder
 
       it 'lists all the packages used in a project' do
         dotnet = Dotnet.new project_path: Pathname.new('app')
-        actual = dotnet.current_packages
 
-        expect(actual.map(&:name)).to match_array %w[Thing1 Thing2 Thing3]
-        expect(actual.map(&:version)).to match_array ['5.2.6', '1.2.3', '5.2.6']
+        expected = [
+            {
+                name: 'Thing1',
+                version: '5.2.6'
+            },
+            {
+                name: 'Thing2',
+                version: '1.2.3'
+            },
+            {
+                name: 'Thing3',
+                version: '5.2.6'
+            }
+        ]
+
+        actual = dotnet.current_packages.map do |package|
+          {name: package.name, version: package.version}
+        end
+
+        expect(actual).to contain_exactly(*expected)
       end
 
       describe 'When an assets file refers to a local project' do
@@ -115,68 +128,17 @@ module LicenseFinder
           expect(actual.map(&:name)).to match_array %w[Thing1 Thing2]
         end
       end
-
-      describe 'When a package has a license URL' do
-        let(:assets_json1) do
-          <<-A1
-          {
-            "libraries": {
-              "Thing1/5.2.6": {
-                "path": "thing1 path/5.2.6",
-                "type": "package",
-                "files": [
-                  "not a nuspec",
-                  "thing1 spec.nuspec"
-                ]
-              }
-            },
-            "packageFolders": {
-              "packages1": {},
-              "packages2": {}
-            }
-          }
-          A1
-        end
-
-        let(:assets_json2) do
-          <<-A2
-          {
-            "libraries": {}
-          }
-          A2
-        end
-
-        let(:thing1_spec) do
-          <<-XML
-          <?xml version="1.0" encoding="utf-8"?>
-          <package xmlns="http://schemas.microsoft.com/packaging/2013/05/nuspec.xsd">
-            <metadata minClientVersion="3.6">
-              <licenseUrl>https://example.com</licenseUrl>
-            </metadata>
-          </package>
-          XML
-        end
-
-        before do
-          FileUtils.mkdir_p('packages1')
-          FileUtils.mkdir_p('packages2/thing1 path/5.2.6/')
-          File.write('packages2/thing1 path/5.2.6/thing1 spec.nuspec', thing1_spec)
-        end
-
-        it 'uses the license URL as the license' do
-          dotnet = Dotnet.new project_path: Pathname.new('app')
-          licenses = dotnet.current_packages[0].license_names_from_spec
-
-          expect(licenses).to eq(['https://example.com'])
-        end
-      end
     end
   end
 
-  describe Dotnet::AssetFile do
-    describe '#packages', skip: LicenseFinder.broken_fakefs? do
-      include FakeFS::SpecHelpers
+  describe Dotnet::AssetFile, skip: LicenseFinder.broken_fakefs? do
+    include FakeFS::SpecHelpers
 
+    before do
+      File.write('project.assets.json', assets_json)
+    end
+
+    describe '#dependencies' do
       let(:assets_json) do
         <<-AJ
         {
@@ -201,25 +163,128 @@ module LicenseFinder
         AJ
       end
 
-      before do
-        File.write('project.assets.json', assets_json)
+      let(:expected_dependencies) do
+        [
+            Dotnet::PackageMetadata.new(
+                'Thing1',
+                '5.2.6',
+                %w(packageFolder1/thing1/5.2.6/foo.nuspec packageFolder2/thing1/5.2.6/foo.nuspec)
+            ),
+            Dotnet::PackageMetadata.new('Thing2', '1.2.3', [])
+        ]
       end
 
       it 'returns the list of packages' do
         asset_file = Dotnet::AssetFile.new('project.assets.json')
-        actual = asset_file.dependencies
-        expected = [
-          Dotnet::PackageMetadata.new(
-            'Thing1',
-            '5.2.6',
-            [
-              'packageFolder1/thing1/5.2.6/foo.nuspec',
-              'packageFolder2/thing1/5.2.6/foo.nuspec'
-            ]
-          ),
-          Dotnet::PackageMetadata.new('Thing2', '1.2.3', [])
-        ]
-        expect(actual).to eq(expected)
+        expect(asset_file.dependencies).to eq(expected_dependencies)
+      end
+    end
+
+    describe '#possible_spec_paths' do
+      describe 'For a package with a nuspec file' do
+        let(:assets_json) do
+          <<-AJ
+          {
+            "version": 3,
+            "libraries": {
+              "Thing1/5.2.6": {
+                "path": "thing1/5.2.6",
+                "type": "package",
+                "files": ["something.dll", "foo.nuspec"]
+              }
+            },
+            "packageFolders": {
+              "packageFolder1": {},
+              "packageFolder2": {}
+            }
+          }
+          AJ
+        end
+
+        it 'returns the nuspec file in each package folder' do
+          asset_file = Dotnet::AssetFile.new('project.assets.json')
+          expected = %w(packageFolder1/thing1/5.2.6/foo.nuspec packageFolder2/thing1/5.2.6/foo.nuspec)
+          expect(asset_file.possible_spec_paths('Thing1/5.2.6')).to eq(expected)
+        end
+      end
+
+      describe 'For a package without a nuspec file' do
+        let(:assets_json) do
+          <<-AJ
+          {
+            "version": 3,
+            "libraries": {
+              "Thing1/5.2.6": {
+                "path": "thing1/5.2.6",
+                "type": "package",
+                "files": []
+              }
+            },
+            "packageFolders": {
+              "packageFolder1": {}
+            }
+          }
+          AJ
+        end
+
+        it 'returns an empty array' do
+          asset_file = Dotnet::AssetFile.new('project.assets.json')
+          expect(asset_file.possible_spec_paths('Thing1/5.2.6')).to be_empty
+        end
+      end
+    end
+  end
+
+  describe Dotnet::PackageMetadata do
+    describe '#read_license_urls', skip: LicenseFinder.broken_fakefs? do
+      include FakeFS::SpecHelpers
+
+      before do
+        File.write('foo.nuspec', nuspec_contents)
+      end
+
+      let(:nuspec_contents) do
+        <<-NUSPEC
+        <?xml version="1.0" encoding="utf-8"?>
+          <package xmlns="http://schemas.microsoft.com/packaging/2013/05/nuspec.xsd">
+            <metadata>
+              <licenseUrl>http://www.microsoft.com/web/webpi/eula/net_library_eula_ENU.htm</licenseUrl>
+            </metadata>
+          </package>
+        NUSPEC
+      end
+
+      it 'returns the license URL from each file that exists' do
+        possible_paths = %w(bar.nuspec foo.nuspec)
+        package_metadata = Dotnet::PackageMetadata.new('arbitrary', 'arbitrary', possible_paths)
+        expected_url = 'http://www.microsoft.com/web/webpi/eula/net_library_eula_ENU.htm'
+        expect(package_metadata.read_license_urls).to eq([expected_url])
+      end
+    end
+
+    describe '#==' do
+      it 'returns true when all attributes are equal' do
+        metadata1 = Dotnet::PackageMetadata.new('A', 1.2, %w(foo bar))
+        metadata2 = Dotnet::PackageMetadata.new('A', 1.2, %w(foo bar))
+        expect(metadata1).to eq(metadata2)
+      end
+
+      it 'returns false when names are different' do
+        metadata1 = Dotnet::PackageMetadata.new('A', 1.2, %w(foo bar))
+        metadata2 = Dotnet::PackageMetadata.new('B', 1.2, %w(foo bar))
+        expect(metadata1).to_not eq(metadata2)
+      end
+
+      it 'returns false when versions are different' do
+        metadata1 = Dotnet::PackageMetadata.new('A', 1.2, %w(foo bar))
+        metadata2 = Dotnet::PackageMetadata.new('A', 1.3, %w(foo bar))
+        expect(metadata1).to_not eq(metadata2)
+      end
+
+      it 'returns false when possible_spec_paths are different' do
+        metadata1 = Dotnet::PackageMetadata.new('A', 1.2, %w(foo bar))
+        metadata2 = Dotnet::PackageMetadata.new('A', 1.2, %w(bar baz))
+        expect(metadata1).to_not eq(metadata2)
       end
     end
   end
---
+++ spec/support/license_finder.rb
@@ -0,0 +1,5 @@
+module LicenseFinder
+  def self.broken_fakefs?
+    RUBY_PLATFORM =~ /java/ || RUBY_VERSION =~ /^(1\.9|2\.0)/
+  end
+end
