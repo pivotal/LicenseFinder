14f939d1e94a9d444783dacc5b38c582f8ae957a
Eric Sullivan <eric.sullivan@annkissam.com>
2019-02-21 08:23:38 -0500

    test just the tests again

--- lib/license_finder/package_managers/bundler.rb
+++ lib/license_finder/package_managers/bundler.rb
@@ -53,17 +53,19 @@ module LicenseFinder
       # clear gem paths before runninng specs_for
       Gem.clear_paths
 
-      ::Bundler.with_original_env do
-        if project_path&.exist?
-          Dir.chdir(project_path) do
-            ::Bundler.configure
-            @gem_details = definition.specs_for(included_groups)
-          end
-        else
-          ::Bundler.configure
-          @gem_details = definition.specs_for(included_groups)
-        end
-      end
+      # ::Bundler.with_original_env do
+      #   if project_path&.exist?
+      #     Dir.chdir(project_path) do
+      #       ::Bundler.configure
+      #       @gem_details = definition.specs_for(included_groups)
+      #     end
+      #   else
+      #     ::Bundler.configure
+      #     @gem_details = definition.specs_for(included_groups)
+      #   end
+      # end
+
+      @gem_details = definition.specs_for(included_groups)
     end
 
     def bundler_details
