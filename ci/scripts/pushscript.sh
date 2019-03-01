5d964801aac09b628048960c42a9070e2ec63c80
Yoon Jean Kim <ykim@pivotal.io>
2019-03-01 12:02:13 -0500

    Fix version names
    Signed-off-by: Shane Lattanzio <slattanzio@pivotal.io>

--- ci/scripts/pushscript.sh
+++ ci/scripts/pushscript.sh
@@ -4,7 +4,7 @@ echo -e "---\n:rubygems_api_key: $GEM_API_KEY" > ~/.gem/credentials
 chmod 0600 ~/.gem/credentials
 
 cd lf-git
-build_version="$(cat version/version)"
+build_version="$(cat semver-version/version)"
 built_gem="pkg/license_finder-$build_version.gem"
 
 git config --global user.email $GIT_EMAIL
