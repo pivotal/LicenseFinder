851a01501cfa57b99a7caca83eb06ff2e5a9a543
Shane Lattanzio <slattanzio@pivotal.io>
2019-06-03 12:01:12 -0400

    [Changed] NPM will now output proper errors

--- spec/lib/license_finder/package_managers/npm_spec.rb
+++ spec/lib/license_finder/package_managers/npm_spec.rb
@@ -35,7 +35,7 @@ module LicenseFinder
         FileUtils.mkdir_p(Dir.tmpdir)
         FileUtils.mkdir_p(root)
         File.write(File.join(root, 'package.json'), package_json)
-        allow(SharedHelpers::Cmd).to receive(:run).with('npm list --json --long')
+        allow(SharedHelpers::Cmd).to receive(:run).with('npm list --json --long | cat')
                                                   .and_return([dependency_json, '', cmd_success])
       end
 
@@ -62,7 +62,7 @@ module LicenseFinder
         FileUtils.mkdir_p(Dir.tmpdir)
         FileUtils.mkdir_p(root)
         File.write(File.join(root, 'package.json'), package_json)
-        allow(SharedHelpers::Cmd).to receive(:run).with('npm list --json --long')
+        allow(SharedHelpers::Cmd).to receive(:run).with('npm list --json --long | cat')
                                                   .and_return([dependency_json, '', cmd_success])
       end
 
@@ -99,8 +99,8 @@ module LicenseFinder
       end
 
       it 'fails when command fails' do
-        allow(SharedHelpers::Cmd).to receive(:run).with('npm list --json --long').and_return ['', 'error', cmd_failure]
-        expect { npm.current_packages }.to raise_error("Command 'npm list --json --long' failed to execute: error")
+        allow(SharedHelpers::Cmd).to receive(:run).with('npm list --json --long | cat').and_return ['', 'error', cmd_failure]
+        expect { npm.current_packages }.to raise_error("Command 'npm list --json --long | cat' failed to execute: error")
       end
 
       it 'does not fail when command fails but produces output' do
@@ -111,7 +111,7 @@ module LicenseFinder
       context 'ignored_groups contains devDependencies' do
         let(:npm) { NPM.new project_path: Pathname.new(root), ignored_groups: 'devDependencies' }
         it 'should include a production flag' do
-          expect(SharedHelpers::Cmd).to receive(:run).with('npm list --json --long --production')
+          expect(SharedHelpers::Cmd).to receive(:run).with('npm list --json --long --production | cat')
                                                      .and_return([dependency_json, '', cmd_success])
           npm.current_packages
         end
