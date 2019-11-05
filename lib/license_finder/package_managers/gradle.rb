08e0df857c7fa4273eb6e2e4a7c01bb46550a91f
Shane Lattanzio <slattanzio@pivotal.io>
2019-11-05 10:55:06 -0500

    [Fixed] Crash when gradle runs with project roots recursive [#169465210]
    Signed-off-by: Jeff Jun <jjun@pivotal.io>

--- lib/license_finder/package_managers/gradle.rb
+++ lib/license_finder/package_managers/gradle.rb
@@ -49,8 +49,18 @@ module LicenseFinder
     private
 
     def root_module?
-      command = "#{package_management_command} properties | grep 'parent: '"
+      return false if project_path.to_s.include?('buildSrc')
+      command = "#{package_management_command} -Dorg.gradle.jvmargs=-Xmx6144m  properties  | grep 'parent: '"
       stdout, stderr, status = Dir.chdir(project_path) { Cmd.run(command) }
+
+      if stderr.include?('not part of the build defined by settings file')
+        Dir.chdir(project_path) do
+          Cmd.run('touch settings.gradle')
+          stdout, stderr, status = Cmd.run(command)
+          Cmd.run('rm settings.gradle')
+        end
+      end
+
       raise "Command '#{command}' failed to execute: #{stderr}" unless status.success?
 
       root_project_name = stdout.gsub(/\s|parent:|\n/, '')
