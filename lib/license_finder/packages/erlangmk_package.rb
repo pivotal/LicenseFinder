eee88f45daf70e79a81d32867ce87eca60408c28
Gerhard Lazu <gerhard@lazu.co.uk>
2020-07-22 18:31:13 +0100

    Simplify ErlangmkPackage
    The logic of various dep parts was repeated multiple times. While the
    validation still feels too complex, it's good enough to ship IMO.
    
    Signed-off-by: Gerhard Lazu <gerhard@lazu.co.uk>

--- lib/license_finder/packages/erlangmk_package.rb
+++ lib/license_finder/packages/erlangmk_package.rb
@@ -7,92 +7,101 @@ end
 
 module LicenseFinder
   class ErlangmkPackage < Package
-    @word_re = Regexp.new('^\w+$')
-    @version_re = Regexp.new('\d+\.\d+\.\d+')
-    @version_prefix_re = Regexp.new('^v')
+    attr_reader :dep_parent,
+                :dep_name,
+                :dep_fetch_method,
+                :dep_repo_unformatted,
+                :dep_version_unformatted,
+                :dep_absolute_path
 
-    class << self
-      def new_from_query_deps(dep)
-        raise_if_not_valid(dep)
-        new(
-          dep_name(dep),
-          dep_version(dep),
-          homepage: dep_repo(dep),
-          install_path: dep_path(dep)
-        )
-      end
+    def initialize(dep_string_from_query_deps)
+      @dep_parent,
+      @dep_name,
+      @dep_fetch_method,
+      @dep_repo_unformatted,
+      @dep_version_unformatted,
+      @dep_absolute_path = dep_string_from_query_deps.split
 
-      def dep_name(dep)
-        _dep_parent, dep_name, _dep_fetch_method, _dep_repo, _dep_version_str, _dep_absolute_path = dep.split
-        dep_name
-      end
+      raise_invalid(dep_string_from_query_deps) unless all_parts_valid?
 
-      def dep_version(dep)
-        _dep_parent, _dep_name, _dep_fetch_method, _dep_repo, dep_version_str, _dep_absolute_path = dep.split
-        fixup_dep_version(dep_version_str)
-      end
-
-      def dep_repo(dep)
-        _dep_parent, _dep_name, _dep_fetch_method, dep_repo, _dep_version_str, _dep_absolute_path = dep.split
-        fixup_dep_repo(dep_repo)
-      end
-
-      def dep_path(dep)
-        _dep_parent, _dep_name, _dep_fetch_method, _dep_repo, _dep_version_str, dep_absolute_path = dep.split
-        dep_absolute_path
-      end
-
-      def raise_if_not_valid(dep)
-        invalid_dep_message = "'#{dep}' does not look like a valid Erlank.mk dependency"
-        valid_dep_example = "A valid dependency example: 'lager: goldrush git https://github.com/DeadZen/goldrush.git 0.1.9 /absolute/path/to/dep'"
-
-        raise(InvalidErlangmkPackageError, "#{invalid_dep_message}. #{valid_dep_example}") unless valid?(dep)
-      end
-
-      def valid?(dep)
-        dep_parent, dep_name, dep_fetch_method, dep_repo, dep_version_str, _dep_absolute_path = dep.split
-        dep_valid?(dep_parent) && dep_valid?(dep_name) && dep_valid?(dep_fetch_method) && dep_repo_valid?(dep_repo) && dep_version_valid?(dep_version_str)
-      end
-
-      private
-
-      def fixup_dep_repo(dep_repo)
-        dep_repo.chomp('.git').sub('git@github.com:', 'https://github.com/')
-      end
-
-      def fixup_dep_version(dep_version_str)
-        dep_version_str.sub(@version_prefix_re, '')
-      end
-
-      def dep_valid?(arg_dep)
-        return false if arg_dep.nil? || arg_dep.empty?
-
-        dep = arg_dep.chomp(':')
-        dep =~ @word_re
-      end
-
-      def dep_repo_valid?(arg_dep_repo)
-        return false if arg_dep_repo.nil? || arg_dep_repo.empty?
-
-        dep_repo = fixup_dep_repo(arg_dep_repo)
-        _dep_repo = URI.parse(dep_repo)
-        true
-      end
-
-      def dep_version_valid?(arg_dep_version_str)
-        return false if arg_dep_version_str.nil? || arg_dep_version_str.empty?
-
-        dep_version_str = fixup_dep_version(arg_dep_version_str)
-        if dep_version_str =~ @version_re
-          Gem::Version.correct?(dep_version_str)
-        else
-          arg_dep_version_str =~ @word_re
-        end
-      end
+      super(
+        dep_name,
+        dep_version,
+        homepage: dep_repo,
+        install_path: dep_absolute_path
+      )
     end
 
     def package_manager
       'Erlangmk'
+    end
+
+    def dep_version
+      @dep_version ||= begin
+        version_prefix_re = Regexp.new('^v')
+        dep_version_unformatted.sub(version_prefix_re, '')
+      end
+    end
+
+    def dep_repo
+      @dep_repo ||= dep_repo_unformatted
+                    .chomp('.git')
+                    .sub('git@github.com:', 'https://github.com/')
+    end
+
+    def raise_invalid(dep_string)
+      invalid_dep_message = "'#{dep_string}' does not look like a valid Erlank.mk dependency"
+      valid_dep_example = "A valid dependency example: 'lager: goldrush git https://github.com/DeadZen/goldrush.git 0.1.9 /absolute/path/to/dep'"
+      raise(InvalidErlangmkPackageError, "#{invalid_dep_message}. #{valid_dep_example}")
+    end
+
+    def all_parts_valid?
+      dep_part_valid?(dep_parent) &&
+      dep_part_valid?(dep_name) &&
+      is_defined?(dep_fetch_method) &&
+      dep_repo_valid? &&
+      dep_version_valid? &&
+      is_defined?(dep_absolute_path)
+    end
+
+    private
+
+    def dep_part_valid?(dep_part)
+      is_defined?(dep_part) &&
+      is_word?(dep_part)
+    end
+
+    def is_defined?(dep_part)
+      !dep_part.nil? &&
+      !dep_part.empty?
+    end
+
+    def is_word?(dep_part)
+      dep = dep_part.chomp(':')
+      dep =~ word_re
+    end
+
+    def word_re
+      @word_re ||= Regexp.new('^\w+$')
+    end
+
+    def dep_repo_valid?
+      is_defined?(dep_repo_unformatted) &&
+      URI.parse(dep_repo)
+    end
+
+    def dep_version_valid?
+      is_defined?(dep_version_unformatted)
+
+      if dep_version =~ version_re
+        Gem::Version.correct?(dep_version)
+      else
+        dep_version =~ word_re
+      end
+    end
+
+    def version_re
+      @version_re ||= Regexp.new('\d+\.\d+\.\d+')
     end
   end
 end
--- spec/lib/license_finder/package_managers/erlangmk_package_spec.rb
+++ spec/lib/license_finder/package_managers/erlangmk_package_spec.rb
@@ -5,7 +5,7 @@ require 'spec_helper'
 module LicenseFinder
   describe ErlangmkPackage do
     subject do
-      described_class.new_from_query_deps(
+      described_class.new(
         'rabbit_common: credentials_obfuscation hex https://hex.pm/packages/credentials_obfuscation 2.0.0' \
         ' /home/lbakken/development/rabbitmq/LicenseFinder/features/fixtures/erlangmk/deps/credentials_obfuscation'
       )
@@ -22,49 +22,35 @@ module LicenseFinder
     its(:package_manager) { should eq 'Erlangmk' }
 
     context 'when public github package https://github.com/rabbitmq/rabbitmq-cli.git' do
-      let(:dep) { 'parent: rabbitmq_cli git https://github.com/rabbitmq/rabbitmq-cli.git v3.8.3-rc.1 /erlangmk/project/path/deps/rabbitmq_cli' }
-
-      describe '.dep_name' do
-        it { expect(described_class.dep_name(dep)).to eql('rabbitmq_cli') }
+      subject do
+        described_class.new(
+          'parent: rabbitmq_cli git https://github.com/rabbitmq/rabbitmq-cli.git v3.8.3-rc.1 /erlangmk/project/path/deps/rabbitmq_cli'
+        )
       end
 
-      describe '.dep_version' do
-        it { expect(described_class.dep_version(dep)).to eql('3.8.3-rc.1') }
-      end
-
-      describe '.dep_repo' do
-        it { expect(described_class.dep_repo(dep)).to eql('https://github.com/rabbitmq/rabbitmq-cli') }
-      end
-
-      describe '.dep_path' do
-        it { expect(described_class.dep_path(dep)).to eql('/erlangmk/project/path/deps/rabbitmq_cli') }
-      end
+      its(:name) { should == 'rabbitmq_cli' }
+      its(:version) { should == '3.8.3-rc.1' }
+      its(:homepage) { should == 'https://github.com/rabbitmq/rabbitmq-cli' }
+      its(:install_path) { should eq '/erlangmk/project/path/deps/rabbitmq_cli' }
     end
 
     context 'when private github package' do
-      let(:dep) { 'parent: zstd git+ssh git@github.com:rabbitmq/zstd-erlang master /erlangmk/project/path/deps/zstd' }
-
-      describe '.dep_name' do
-        it { expect(described_class.dep_name(dep)).to eql('zstd') }
+      subject do
+        described_class.new(
+          'parent: zstd git+ssh git@github.com:rabbitmq/zstd-erlang master /erlangmk/project/path/deps/zstd'
+        )
       end
 
-      describe '.dep_version' do
-        it { expect(described_class.dep_version(dep)).to eql('master') }
-      end
-
-      describe '.dep_repo' do
-        it { expect(described_class.dep_repo(dep)).to eql('https://github.com/rabbitmq/zstd-erlang') }
-      end
-
-      describe '.dep_path' do
-        it { expect(described_class.dep_path(dep)).to eql('/erlangmk/project/path/deps/zstd') }
-      end
+      its(:name) { should == 'zstd' }
+      its(:version) { should == 'master' }
+      its(:homepage) { should == 'https://github.com/rabbitmq/zstd-erlang' }
+      its(:install_path) { should eq '/erlangmk/project/path/deps/zstd' }
     end
 
     describe 'guards against invalid packages' do
       context 'when empty string' do
         it do
-          expect { described_class.new_from_query_deps('') }.to raise_error(InvalidErlangmkPackageError)
+          expect { described_class.new('') }.to raise_error(InvalidErlangmkPackageError)
         end
       end
     end
