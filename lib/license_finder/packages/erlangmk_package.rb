97fd019b53d4a105952307d978de7ebda0ad61fc
Gerhard Lazu <gerhard@lazu.co.uk>
2020-07-22 17:23:33 +0100

    Remove redundant NOTE comment
    This captures what @lukebakken was thinking here:
    https://github.com/pivotal/LicenseFinder/pull/671#discussion_r458417348
    
    The deps private method in the Erlangmk PackageManager is the source of
    truth for how this works, and the specs are a good place for start.
    
    Signed-off-by: Gerhard Lazu <gerhard@lazu.co.uk>

--- lib/license_finder/packages/erlangmk_package.rb
+++ lib/license_finder/packages/erlangmk_package.rb
@@ -5,20 +5,6 @@ require 'rubygems'
 class InvalidErlangmkPackageError < ArgumentError
 end
 
-# NOTE:
-# Command:
-#   make QUERY='name fetch_method repo version absolute_path' query-deps
-# Output:
-# make[1]: Entering directory '/home/lbakken/development/rabbitmq/umbrella/deps/credentials_obfuscation'
-# make[1]: Leaving directory '/home/lbakken/development/rabbitmq/umbrella/deps/credentials_obfuscation'
-# rabbit_common: rabbitmq_codegen git_rmq https://github.com/rabbitmq/rabbitmq-codegen master /home/lbakken/development/rabbitmq/umbrella/deps/rabbitmq_codegen
-# rabbit_common: lager hex https://hex.pm/packages/lager 3.8.0 /home/lbakken/development/rabbitmq/umbrella/deps/lager
-# rabbit_common: jsx hex https://hex.pm/packages/jsx 2.9.0 /home/lbakken/development/rabbitmq/umbrella/deps/jsx
-# rabbit_common: ranch hex https://hex.pm/packages/ranch 1.7.1 /home/lbakken/development/rabbitmq/umbrella/deps/ranch
-# rabbit_common: recon hex https://hex.pm/packages/recon 2.5.1 /home/lbakken/development/rabbitmq/umbrella/deps/recon
-# rabbit_common: credentials_obfuscation hex https://hex.pm/packages/credentials_obfuscation 2.0.0 /home/lbakken/development/rabbitmq/umbrella/deps/credentials_obfuscation
-# lager: goldrush git https://github.com/DeadZen/goldrush.git 0.1.9 /home/lbakken/development/rabbitmq/umbrella/deps/goldrush
-
 module LicenseFinder
   class ErlangmkPackage < Package
     @word_re = Regexp.new('^\w+$')
