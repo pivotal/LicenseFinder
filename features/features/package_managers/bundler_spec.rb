9339b1baa9f62525a27f291480ab5811cd40d173
Francesco Belladonna <francesco@fc5.me>
2021-07-07 14:20:40 -0700

    Changed bundler package manager licenses retrieval from external process

--- features/features/package_managers/bundler_spec.rb
+++ features/features/package_managers/bundler_spec.rb
@@ -4,11 +4,17 @@ require_relative '../../support/feature_helper'
 require_relative '../../../lib/license_finder/version'
 
 describe 'Bundler Dependencies' do
+  around do |example|
+    # Run license_finder outside any Bundler environment
+    ::Bundler.with_unbundled_env do
+      example.run
+    end
+  end
+
   let(:bundler_developer) { LicenseFinder::TestingDSL::User.new }
 
   specify 'are shown in reports' do
     LicenseFinder::TestingDSL::BundlerProject.create
-    puts 'bundler project created'
     bundler_developer.run_license_finder
     expect(bundler_developer).to be_seeing_something_like /bundler.*MIT/
   end
@@ -21,18 +27,11 @@ describe 'Bundler Dependencies' do
   end
 
   specify 'works with git dependency running outside of the bundle' do
-    # Run license_finder outside any Bundler environment
-    ::Bundler.with_unbundled_env do
-      # Fake home to avoid polluting real home with git checkout
-      ENV['HOME'] = LicenseFinder::TestingDSL::Paths.projects.to_s
-      # Make running license_finder pick up the correct implementation
-      ENV['RUBYOPT'] = "-I#{LicenseFinder::TestingDSL::Paths.root}/lib"
-      # Allow the main executable to be found
-      ENV['PATH'] = "#{LicenseFinder::TestingDSL::Paths.root}/bin:#{ENV['PATH']}"
-      LicenseFinder::TestingDSL::GitBundlerProject.create
-      puts 'bundler project created'
-      bundler_developer.run_license_finder
-      expect(bundler_developer).to be_seeing_something_like /bundler.*MIT/
-    end
+    # Fake home to avoid polluting real home with git checkout
+    ENV['HOME'] = LicenseFinder::TestingDSL::Paths.projects.to_s
+    LicenseFinder::TestingDSL::GitBundlerProject.create
+    puts 'bundler project created'
+    bundler_developer.run_license_finder
+    expect(bundler_developer).to be_seeing_something_like /bundler.*MIT/
   end
 end
