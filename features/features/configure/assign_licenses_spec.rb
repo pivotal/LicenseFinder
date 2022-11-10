be4c32baa2dbfd691d35d6bb4e4e16da06abbd94
Michael O'Neil <mtoneil@gmail.com>
2022-11-10 11:50:08 -0500

    Change implementation of how removing a license should work
    This makes the license optional when removing a license. When omitted,
    all licenses will be removed from the relevant package versions.

--- features/features/configure/assign_licenses_spec.rb
+++ features/features/configure/assign_licenses_spec.rb
@@ -14,6 +14,7 @@ describe 'Manually Assigned Licenses' do
     gem = developer.create_gem 'mislicensed_dep', license: 'Unknown'
     project.depend_on gem
     developer.execute_command 'license_finder licenses add mislicensed_dep Known'
+    expect(developer).to be_seeing('The mislicensed_dep dependency has been marked as using Known license!')
 
     developer.run_license_finder
     expect(developer).not_to be_seeing_something_like /mislicensed_dep.*Unknown/
@@ -22,17 +23,33 @@ describe 'Manually Assigned Licenses' do
 
   specify 'can be removed, revealing the default license for a dependency' do
     project = developer.create_ruby_app
-    gem = developer.create_gem 'mislicensed_dep', license: 'Default'
-    project.depend_on gem
-    developer.execute_command 'license_finder licenses add mislicensed_dep Manual_license'
+    gem_a = developer.create_gem 'mislicensed_dep_a', license: 'Default'
+    gem_b = developer.create_gem 'mislicensed_dep_b', license: 'Default'
+    project.depend_on gem_a
+    project.depend_on gem_b
+    developer.execute_command 'license_finder licenses add mislicensed_dep_a Manual_license_a'
+    developer.execute_command 'license_finder licenses add mislicensed_dep_b Manual_license_b'
 
     developer.run_license_finder
-    expect(developer).to be_seeing_something_like /mislicensed_dep.*Manual_license/
+    expect(developer).to be_seeing_something_like /mislicensed_dep_a.*Manual_license_a/
+    expect(developer).to be_seeing_something_like /mislicensed_dep_b.*Manual_license_b/
 
-    developer.execute_command 'license_finder licenses remove mislicensed_dep Manual_license'
+    developer.execute_command 'license_finder licenses remove mislicensed_dep_a Manual_license_a'
+    expect(developer).to be_seeing('The dependency mislicensed_dep_a no longer has a manual license of Manual_license_a')
+
+    developer.execute_command 'license_finder licenses remove mislicensed_dep_b Incorrect_license'
+    expect(developer).to be_seeing('The dependency mislicensed_dep_b no longer has a manual license of Incorrect_license')
 
     developer.run_license_finder
-    expect(developer).to be_seeing_something_like /mislicensed_dep.*Default/
+    expect(developer).to be_seeing_something_like /mislicensed_dep_a.*Default/
+    expect(developer).to be_seeing_something_like /mislicensed_dep_b.*Manual_license_b/
+
+    developer.execute_command 'license_finder licenses remove mislicensed_dep_b'
+    expect(developer).to be_seeing('The dependency mislicensed_dep_b no longer has a manual license')
+
+    developer.run_license_finder
+    expect(developer).to be_seeing_something_like /mislicensed_dep_a.*Default/
+    expect(developer).to be_seeing_something_like /mislicensed_dep_b.*Default/
   end
 
   specify 'can be assigned and removed by package version' do
@@ -45,6 +62,7 @@ describe 'Manually Assigned Licenses' do
     developer.execute_command 'license_finder licenses add dep_a GPL --version=1.0.0'
     developer.execute_command 'license_finder licenses add dep_a MIT --version=2.0.0'
     developer.execute_command 'license_finder licenses add dep_a GPL --version=3.0.0'
+    expect(developer).to be_seeing('The dep_a dependency with version 3.0.0 has been marked as using GPL license!')
 
     developer.execute_command 'license_finder licenses add dep_b GPL --version=1.0.0'
     developer.execute_command 'license_finder licenses add dep_b GPL --version=2.0.0'
@@ -55,9 +73,17 @@ describe 'Manually Assigned Licenses' do
     expect(developer).to be_seeing_line 'dep_b, 3.0.0, "Apache 2.0"'
 
     developer.execute_command 'license_finder licenses remove dep_a MIT --version=2.0.0'
+    expect(developer).to be_seeing('The dependency dep_a with version 2.0.0 no longer has a manual license of MIT')
 
     developer.run_license_finder
     expect(developer).to be_seeing_line 'dep_a, 2.0.0, Unknown'
     expect(developer).to be_seeing_line 'dep_b, 3.0.0, "Apache 2.0"'
+
+    developer.execute_command 'license_finder licenses remove dep_b --version=3.0.0'
+    expect(developer).to be_seeing('The dependency dep_b with version 3.0.0 no longer has a manual license')
+
+    developer.run_license_finder
+    expect(developer).to be_seeing_line 'dep_a, 2.0.0, Unknown'
+    expect(developer).to be_seeing_line 'dep_b, 3.0.0, Unknown'
   end
 end
